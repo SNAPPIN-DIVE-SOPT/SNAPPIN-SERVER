ad3f738aae408a788a68e7cf89652ba772302a03
kaswhy <sxshx818@naver.com>
2025-12-27 16:06:41 +0900

    init(#0/*): 프로젝트 기본 설정

---
+++ src/main/java/org/sopt/snappinserver/SnappinServerApplication.java
@@ -0,0 +1,13 @@
+package org.sopt.snappinserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnappinServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SnappinServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/sopt/snappinserver/SnappinServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sopt.snappinserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnappinServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
