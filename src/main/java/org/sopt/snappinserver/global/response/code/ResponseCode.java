7d23b5c11a30582d8be17cedaa7bfbe453672c69
kaswhy <sxshx818@naver.com>
2025-12-29 20:52:01 +0900

    feat(#1/global): 응답 코드 구현

---
+++ src/main/java/org/sopt/snappinserver/global/response/code/ResponseCode.java
@@ -0,0 +1,7 @@
+package org.sopt.snappinserver.global.response.code;
+
+public interface ResponseCode {
+    int getStatus();
+    String getCode();
+    String getMessage();
+}
---
+++ src/main/java/org/sopt/snappinserver/global/response/code/SuccessCode.java
@@ -0,0 +1,4 @@
+package org.sopt.snappinserver.global.response.code;
+
+public interface SuccessCode extends ResponseCode {
+}
