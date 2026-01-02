fc5b7313ff500bc906f3c70d6bcffe411c94b2b5
kaswhy <sxshx818@naver.com>
2026-01-02 21:49:31 +0900

    feat(#3/photo): photo 도메인 관련 스켈레톤 코드 작성

---
+++ src/main/java/org/sopt/snappinserver/domain/photo/controller/PhotoApi.java
@@ -0,0 +1,8 @@
+package org.sopt.snappinserver.domain.photo.controller;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name = "03 - Photo", description = "사진 관련 API")
+public interface PhotoApi {
+
+}
