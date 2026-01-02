32ef5572d612d6df3392bdf9c9ba835f161722a4
kaswhy <sxshx818@naver.com>
2026-01-03 00:13:35 +0900

    fix(#3/place): availableLocationRepository 네이밍 수정

--- src/main/java/org/sopt/snappinserver/domain/place/repository/AvailableLocation.java
+++
@@ -1,7 +0,0 @@
-package org.sopt.snappinserver.domain.place.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AvailableLocation extends JpaRepository<AvailableLocation, Long> {
-
-}
---
+++ src/main/java/org/sopt/snappinserver/domain/place/repository/AvailableLocationRepository.java
@@ -0,0 +1,8 @@
+package org.sopt.snappinserver.domain.place.repository;
+
+import org.sopt.snappinserver.domain.place.domain.entity.AvailableLocation;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AvailableLocationRepository extends JpaRepository<AvailableLocation, Long> {
+
+}
