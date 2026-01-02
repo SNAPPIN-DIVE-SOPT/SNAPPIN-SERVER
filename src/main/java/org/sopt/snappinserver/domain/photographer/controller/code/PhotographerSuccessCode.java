ce94f447e2bca9e819fad1cbc7bfde9fd15c7169
kaswhy <sxshx818@naver.com>
2026-01-02 17:52:16 +0900

    feat(#3/photographer): photographer 도메인 관련 스켈레톤 코드 작성

---
+++ src/main/java/org/sopt/snappinserver/domain/photographer/controller/code/PhotographerSuccessCode.java
@@ -0,0 +1,20 @@
+package org.sopt.snappinserver.domain.photographer.controller.code;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.sopt.snappinserver.global.response.code.SuccessCode;
+
+@Getter
+@RequiredArgsConstructor
+public enum PhotographerSuccessCode implements SuccessCode {
+
+    // 200 OK
+
+    // 201 CREATED
+
+    ;
+
+    private final int status;
+    private final String code;
+    private final String message;
+}
---
+++ src/main/java/org/sopt/snappinserver/domain/photographer/repository/PhotographerSchedule.java
@@ -0,0 +1,7 @@
+package org.sopt.snappinserver.domain.photographer.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PhotographerSchedule extends JpaRepository<PhotographerSchedule, Long> {
+
+}
