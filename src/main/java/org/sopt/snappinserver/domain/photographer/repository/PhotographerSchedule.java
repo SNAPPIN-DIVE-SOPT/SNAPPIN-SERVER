5ad35000511ad71a7329e9eb84da8c37ec95136d
kaswhy <sxshx818@naver.com>
2026-01-02 18:20:01 +0900

    fix(#3/photographer): PhotographerScheduleRepository 인터페이스 이름 변경

--- src/main/java/org/sopt/snappinserver/domain/photographer/repository/PhotographerSchedule.java
+++
@@ -1,7 +0,0 @@
-package org.sopt.snappinserver.domain.photographer.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PhotographerSchedule extends JpaRepository<PhotographerSchedule, Long> {
-
-}
