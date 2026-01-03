53b20ea4ff37a43aabd01e546daca520fbbffcdf
kaswhy <sxshx818@naver.com>
2026-01-04 02:10:04 +0900

    feat(#3/wish): wish 도메인 관련 controller 단 스켈레톤 코드 작성

---
+++ src/main/java/org/sopt/snappinserver/api/wish/controller/WishController.java
@@ -0,0 +1,12 @@
+package org.sopt.snappinserver.api.wish.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/api/v1/wishes")
+@RequiredArgsConstructor
+@RestController
+public class WishController {
+
+}
