1e0002bb0aea133f5867d3912752c08352c7a352
kaswhy <sxshx818@naver.com>
2026-01-04 01:54:51 +0900

    refactor(#3/photo): photo 관련 컨트롤러 단 스켈레톤 코드 삭제

--- src/main/java/org/sopt/snappinserver/domain/photo/controller/PhotoApi.java
+++
@@ -1,8 +0,0 @@
-package org.sopt.snappinserver.domain.photo.controller;
-
-import io.swagger.v3.oas.annotations.tags.Tag;
-
-@Tag(name = "03 - Photo", description = "사진 관련 API")
-public interface PhotoApi {
-
-}
