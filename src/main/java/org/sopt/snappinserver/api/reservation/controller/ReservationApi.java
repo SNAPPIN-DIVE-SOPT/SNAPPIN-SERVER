f077bcc132912753eb45207224f2ed3382b7d415
kaswhy <sxshx818@naver.com>
2026-01-04 02:06:09 +0900

    feat(#3/reservation): reservation 도메인 관련 controller 단 스켈레톤 코드 작성

---
+++ src/main/java/org/sopt/snappinserver/api/reservation/controller/ReservationApi.java
@@ -0,0 +1,8 @@
+package org.sopt.snappinserver.api.reservation.controller;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name = "010 - Reservation", description = "예약 관련 API")
+public interface ReservationApi {
+
+}
