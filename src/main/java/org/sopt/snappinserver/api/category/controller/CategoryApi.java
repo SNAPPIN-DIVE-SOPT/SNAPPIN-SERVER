7b6f9d00a792b4b6714b3030b29cfb5bc15156c1
kaswhy <sxshx818@naver.com>
2026-01-04 01:09:15 +0900

    feat(#3/category): category api 관련 controller 단 스켈레톤 코드 작성

---
+++ src/main/java/org/sopt/snappinserver/api/category/controller/CategoryApi.java
@@ -0,0 +1,8 @@
+package org.sopt.snappinserver.api.category.controller;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name = "02 - Category", description = "촬영 상황 관련 API")
+public interface CategoryApi {
+
+}
---
+++ src/main/java/org/sopt/snappinserver/api/category/controller/CategoryController.java
@@ -0,0 +1,12 @@
+package org.sopt.snappinserver.api.category.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/api/v1/categories")
+@RequiredArgsConstructor
+@RestController
+public class CategoryController {
+
+}
