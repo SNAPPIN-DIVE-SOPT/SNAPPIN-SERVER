11e1aa9be8470d869a1873942d04b7cf970aa71b
kaswhy <sxshx818@naver.com>
2026-01-04 02:03:47 +0900

    feat(#3/portfolio): portfolio 도메인 관련 controller 단 스켈레톤 코드 작성

---
+++ src/main/java/org/sopt/snappinserver/api/portfolio/controller/PortfolioApi.java
@@ -0,0 +1,8 @@
+package org.sopt.snappinserver.api.portfolio.controller;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name = "08 - Portfolio", description = "포트폴리와 관련 API")
+public interface PortfolioApi {
+
+}
