c31f9e5692061f3af231c35eb67655b6d421e409
kaswhy <sxshx818@naver.com>
2026-01-05 21:38:40 +0900

    feat(#3/product): product 중간 연결 테이블 추가

---
+++ src/main/java/org/sopt/snappinserver/domain/product/domain/entity/ProductPhoto.java
@@ -0,0 +1,51 @@
+package org.sopt.snappinserver.domain.product.domain.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.sopt.snappinserver.domain.photo.domain.entity.Photo;
+import org.sopt.snappinserver.global.entity.BaseEntity;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class ProductPhoto extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "photo_id", nullable = false)
+    private Photo photo;
+
+    @Column(nullable = false)
+    private int order;
+
+    @Builder(access = AccessLevel.PRIVATE)
+    private ProductPhoto(Product product, Photo photo) {
+        this.product = product;
+        this.photo = photo;
+    }
+
+    public ProductPhoto create(Product product, Photo photo) {
+        return ProductPhoto.builder()
+            .product(product)
+            .photo(photo)
+            .build();
+    }
+
+}
