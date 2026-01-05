0bc8d4366915d95809681c7264ec850fa4767b97
kaswhy <sxshx818@naver.com>
2026-01-05 19:32:48 +0900

    feat(#3/reservation): ReservationRepository repository 어노테이션 추가

--- src/main/java/org/sopt/snappinserver/domain/reservation/repository/ReservationRepository.java
+++ src/main/java/org/sopt/snappinserver/domain/reservation/repository/ReservationRepository.java
@@ -2,7 +2,9 @@ package org.sopt.snappinserver.domain.reservation.repository;
 
 import org.sopt.snappinserver.domain.reservation.domain.entity.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
 
 }
