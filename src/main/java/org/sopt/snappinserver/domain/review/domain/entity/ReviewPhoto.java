147607d01c2ddabf34d4ff9c3e198d5cb2ae06b9
kaswhy <sxshx818@naver.com>
2026-01-05 22:03:20 +0900

    feat(#3/review): review 중간 연결 테이블 추가

---
+++ src/main/java/org/sopt/snappinserver/domain/review/domain/entity/ReviewPhoto.java
@@ -0,0 +1,53 @@
+package org.sopt.snappinserver.domain.review.domain.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.sopt.snappinserver.domain.photo.domain.entity.Photo;
+import org.sopt.snappinserver.global.entity.BaseEntity;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class ReviewPhoto extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "review_id", nullable = false)
+    private Review review;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "photo_id", nullable = false)
+    private Photo photo;
+
+    @Column(nullable = false)
+    private int displayOrder;
+
+    @Builder(access = AccessLevel.PRIVATE)
+    private ReviewPhoto(Review review, Photo photo, int displayOrder) {
+        this.review = review;
+        this.photo = photo;
+        this.displayOrder = displayOrder;
+    }
+
+    public static ReviewPhoto create(Review review, Photo photo, int displayOrder) {
+        return ReviewPhoto.builder()
+            .review(review)
+            .photo(photo)
+            .displayOrder(displayOrder)
+            .build();
+    }
+
+}
