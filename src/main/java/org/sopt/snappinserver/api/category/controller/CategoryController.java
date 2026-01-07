4dff563dd549754f4b4e83576543311173131154
earl9rey <earl9rey@naver.com>
2026-01-07 13:41:00 +0900

    fix(#20/category): 카테고리 전체 조회 API 구현

--- src/main/java/org/sopt/snappinserver/api/category/controller/CategoryController.java
+++ src/main/java/org/sopt/snappinserver/api/category/controller/CategoryController.java
@@ -1,12 +1,26 @@
 package org.sopt.snappinserver.api.category.controller;
 
+import java.util.Arrays;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.sopt.snappinserver.api.category.dto.response.CategoriesResponse;
+import org.sopt.snappinserver.api.category.dto.response.CategoryResponse;
+import org.sopt.snappinserver.global.enums.SnapCategory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/api/v1/categories")
 @RequiredArgsConstructor
 @RestController
-public class CategoryController {
+public class CategoryController implements CategoryApi {
 
+    @Override
+    public CategoriesResponse getCategories() {
+        List<CategoryResponse> categories =
+                Arrays.stream(SnapCategory.values())
+                        .map(CategoryResponse::from)
+                        .toList();
+
+        return CategoriesResponse.from(categories);
+    }
 }
