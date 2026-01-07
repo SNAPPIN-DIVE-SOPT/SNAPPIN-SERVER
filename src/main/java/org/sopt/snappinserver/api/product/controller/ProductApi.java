43699af880b38605377746b86cb5ef90e98718d9
earl9rey <earl9rey@naver.com>
2026-01-08 01:32:19 +0900

    refactor(#26/product): 컨트롤러 반환 타입 변경

--- src/main/java/org/sopt/snappinserver/api/product/controller/ProductApi.java
+++ src/main/java/org/sopt/snappinserver/api/product/controller/ProductApi.java
@@ -3,6 +3,7 @@ package org.sopt.snappinserver.api.product.controller;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.sopt.snappinserver.api.product.dto.response.ProductReviewsCursorResponse;
+import org.sopt.snappinserver.api.product.dto.response.ProductReviewsMetaResponse;
 import org.sopt.snappinserver.global.response.dto.ApiResponseBody;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,7 +17,8 @@ public interface ProductApi {
         description = "커서 기반 페이지네이션 방식으로 상품 리뷰 목록을 조회합니다."
     )
     @GetMapping("/{productId}/reviews")
-    ApiResponseBody<ProductReviewsCursorResponse, Void> getProductReviews(
+    ApiResponseBody<ProductReviewsCursorResponse, ProductReviewsMetaResponse>
+    getProductReviews(
         @PathVariable Long productId,
         @RequestParam(required = false) Long cursor
     );
