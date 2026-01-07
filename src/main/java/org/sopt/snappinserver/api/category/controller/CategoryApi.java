201d64675787921d9e9912ca01871353b3be6a2f
earl9rey <earl9rey@naver.com>
2026-01-07 13:40:45 +0900

    fix(#20/category): swagger 명세 인터페이스 추가

--- src/main/java/org/sopt/snappinserver/api/category/controller/CategoryApi.java
+++ src/main/java/org/sopt/snappinserver/api/category/controller/CategoryApi.java
@@ -1,8 +1,17 @@
 package org.sopt.snappinserver.api.category.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.sopt.snappinserver.api.category.dto.response.CategoriesResponse;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Tag(name = "02 - Category", description = "촬영 상황 관련 API")
 public interface CategoryApi {
 
+    @Operation(
+            summary = "촬영 상황 조회",
+            description = "촬영 상황 옵션으로 사용될 스냅 유형 전체 목록을 조회합니다."
+    )
+    @GetMapping
+    CategoriesResponse getCategories();
 }
