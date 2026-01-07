c17872dccd8f92243c9749494d430bffa982896e
earl9rey <earl9rey@naver.com>
2026-01-07 20:19:44 +0900

    feat(#26/product): 상품 리뷰 관련 성공 코드 추가

---
+++ src/main/java/org/sopt/snappinserver/api/product/code/ProductSuccessCode.java
@@ -0,0 +1,20 @@
+package org.sopt.snappinserver.api.product.code;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.sopt.snappinserver.global.response.code.common.SuccessCode;
+
+@Getter
+@RequiredArgsConstructor
+public enum ProductSuccessCode implements SuccessCode {
+
+    GET_PRODUCT_REVIEWS_OK(
+            200,
+            "PRODUCT_200_001",
+            "상품 리뷰 목록 조회에 성공했습니다."
+    );
+
+    private final int status;
+    private final String code;
+    private final String message;
+}
