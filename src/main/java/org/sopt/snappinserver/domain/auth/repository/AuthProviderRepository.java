33e01e3e28f04016892eaef712f40774cde7473b
kaswhy <sxshx818@naver.com>
2026-01-08 02:16:49 +0900

    feat(#27/auth): 유저 조회 후 생성 또는 반환 코드 작성

--- src/main/java/org/sopt/snappinserver/domain/auth/repository/AuthProviderRepository.java
+++ src/main/java/org/sopt/snappinserver/domain/auth/repository/AuthProviderRepository.java
@@ -1,8 +1,14 @@
 package org.sopt.snappinserver.domain.auth.repository;
 
+import java.util.Optional;
 import org.sopt.snappinserver.domain.auth.domain.entity.AuthProvider;
+import org.sopt.snappinserver.domain.auth.domain.enums.SocialProvider;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AuthProviderRepository extends JpaRepository<AuthProvider, Long> {
 
+    Optional<AuthProvider> findBySocialProviderAndProviderId(
+        SocialProvider socialProvider,
+        String providerId
+    );
 }
