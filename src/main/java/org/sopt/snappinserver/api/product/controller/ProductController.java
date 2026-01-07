969bee3324c7cd128211c69f60f92a1e48ea551a
earl9rey <earl9rey@naver.com>
2026-01-07 20:16:41 +0900

    feat(#26/product): 상품 리뷰 조회 엔드포인트 구현 및 공통 응답 래핑

--- src/main/java/org/sopt/snappinserver/api/product/controller/ProductController.java
+++ src/main/java/org/sopt/snappinserver/api/product/controller/ProductController.java
@@ -1,12 +1,30 @@
 package org.sopt.snappinserver.api.product.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.sopt.snappinserver.api.category.code.CategorySuccessCode;
+import org.sopt.snappinserver.api.product.code.ProductSuccessCode;
+import org.sopt.snappinserver.api.product.dto.response.ProductReviewsCursorResponse;
+import org.sopt.snappinserver.domain.product.service.GetProductReviewsUseCase;
+import org.sopt.snappinserver.global.response.dto.ApiResponseBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/api/v1/products")
 @RequiredArgsConstructor
 @RestController
-public class ProductController {
+public class ProductController implements ProductApi {
 
+    private final GetProductReviewsUseCase getProductReviewsUseCase;
+
+    @Override
+    public ApiResponseBody<ProductReviewsCursorResponse, Void> getProductReviews(
+            Long productId,
+            Long cursor
+    ) {
+        ProductReviewsCursorResponse response =
+                getProductReviewsUseCase.getProductReviews(productId, cursor);
+
+        return ApiResponseBody.ok(ProductSuccessCode.GET_PRODUCT_REVIEWS_OK, response);
+    }
 }
+
