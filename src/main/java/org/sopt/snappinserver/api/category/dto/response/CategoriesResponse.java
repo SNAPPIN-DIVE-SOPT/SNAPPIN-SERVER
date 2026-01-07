6c3512f9d09fcce7f5496e31c6e29edda2b6689e
earl9rey <earl9rey@naver.com>
2026-01-07 17:03:53 +0900

    feat(#20/category): 카테고리 목록 DTO 스키마 추가

--- src/main/java/org/sopt/snappinserver/api/category/dto/response/CategoriesResponse.java
+++ src/main/java/org/sopt/snappinserver/api/category/dto/response/CategoriesResponse.java
@@ -1,5 +1,6 @@
 package org.sopt.snappinserver.api.category.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -7,8 +8,10 @@ import java.util.List;
 
 @Getter
 @AllArgsConstructor
+@Schema(description = "촬영 상황 카테고리 목록 응답 DTO")
 public class CategoriesResponse {
 
+    @Schema(description = "촬영 상황 카테고리 목록")
     private List<CategoryResponse> categories;
 
     public static CategoriesResponse from(List<CategoryResponse> categories) {
