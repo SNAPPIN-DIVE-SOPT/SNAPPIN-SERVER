8789cd2979b04bede27c8d87a398cc95cba5260a
earl9rey <earl9rey@naver.com>
2026-01-07 20:50:07 +0900

    feat(#26/product): 커서 기반 상품 리뷰 조회를 위한 레포지토리 추가

--- src/main/java/org/sopt/snappinserver/domain/review/repository/ReviewRepository.java
+++ src/main/java/org/sopt/snappinserver/domain/review/repository/ReviewRepository.java
@@ -4,7 +4,18 @@ import org.sopt.snappinserver.domain.review.domain.entity.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+    // 첫 페이지
+    List<Review> findTop6ByReservationProductIdOrderByIdDesc(
+            Long productId
+    );
 
+    // 커서 이후 페이지
+    List<Review> findTop6ByReservationProductIdAndIdLessThanOrderByIdDesc(
+            Long productId,
+            Long cursor
+    );
 }
