ff1fdf1cbcd08c2aa8f56fe79e199cd6a7163fc6
earl9rey <earl9rey@naver.com>
2026-01-07 22:18:41 +0900

    refactor(#26/product): 상품 리뷰 DTO from 메서드 내부 로직 분리

--- src/main/java/org/sopt/snappinserver/api/product/dto/response/ProductReviewResponse.java
+++ src/main/java/org/sopt/snappinserver/api/product/dto/response/ProductReviewResponse.java
@@ -2,9 +2,13 @@ package org.sopt.snappinserver.api.product.dto.response;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Comparator;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.sopt.snappinserver.domain.review.domain.entity.Review;
+import org.sopt.snappinserver.domain.review.domain.entity.ReviewPhoto;
 
 @Getter
 @AllArgsConstructor
@@ -28,4 +32,34 @@ public class ProductReviewResponse {
 
     @Schema(description = "리뷰 내용", example = "리뷰 내용")
     private String content;
+
+    public static ProductReviewResponse from(Review review) {
+        return new ProductReviewResponse(
+                review.getId(),
+                getReviewerFrom(review),
+                review.getRating(),
+                getCreatedDateFrom(review),
+                getImagesFrom(review),
+                review.getContent()
+        );
+    }
+
+    private static String getReviewerFrom(Review review) {
+        return review.getReservation()
+                .getUser()
+                .getName();
+    }
+
+    private static LocalDate getCreatedDateFrom(Review review) {
+        return review.getCreatedAt()
+                .atZone(ZoneId.systemDefault())
+                .toLocalDate();
+    }
+
+    private static List<String> getImagesFrom(Review review) {
+        return review.getReviewPhotos().stream()
+                .sorted(Comparator.comparingInt(ReviewPhoto::getDisplayOrder))
+                .map(rp -> rp.getPhoto().getImageUrl())
+                .toList();
+    }
 }
