8ce6d024aec5e76b64823c9c39b93bc531e38c2e
kaswhy <sxshx818@naver.com>
2026-01-08 02:12:02 +0900

    feat(#27/auth): 기존 리프레시 토큰 관련 엔티티, 리포지토리 삭제

--- src/main/java/org/sopt/snappinserver/domain/auth/repository/RefreshTokenRepository.java
+++
@@ -1,8 +0,0 @@
-package org.sopt.snappinserver.domain.auth.repository;
-
-import org.sopt.snappinserver.domain.auth.domain.entity.RefreshToken;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
-
-}
