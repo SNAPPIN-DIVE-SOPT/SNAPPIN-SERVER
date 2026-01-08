68c00c4774cb85e2209c5005fd4aca8e0399d9f2
earl9rey <earl9rey@naver.com>
2026-01-08 23:44:57 +0900

    refactor(#26/review): 리뷰 조회 시 Reservation, User fetch join 적용

--- src/main/java/org/sopt/snappinserver/domain/review/repository/ReviewRepository.java
+++ src/main/java/org/sopt/snappinserver/domain/review/repository/ReviewRepository.java
@@ -1,22 +1,41 @@
 package org.sopt.snappinserver.domain.review.repository;
 
+import java.util.List;
 import org.sopt.snappinserver.domain.review.domain.entity.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface ReviewRepository extends JpaRepository<Review, Long> {
 
-    // 첫 페이지
-    List<Review> findTop6ByReservationProductIdOrderByIdDesc(
-        Long productId
+
+    // 첫 페이지 조회 (cursor 없음)
+    @Query("""
+        select r
+        from Review r
+        join fetch r.reservation res
+        join fetch res.user u
+        where res.product.id = :productId
+        order by r.id desc
+    """)
+    List<Review> findTop6WithUserByProductId(
+        @Param("productId") Long productId
     );
 
-    // 커서 이후 페이지
-    List<Review> findTop6ByReservationProductIdAndIdLessThanOrderByIdDesc(
-        Long productId,
-        Long cursor
+    // 커서 이후 페이지 조회
+    @Query("""
+        select r
+        from Review r
+        join fetch r.reservation res
+        join fetch res.user u
+        where res.product.id = :productId
+          and r.id < :cursor
+        order by r.id desc
+    """)
+    List<Review> findTop6WithUserByProductIdAndCursor(
+        @Param("productId") Long productId,
+        @Param("cursor") Long cursor
     );
 }
