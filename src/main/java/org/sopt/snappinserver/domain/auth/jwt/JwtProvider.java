42e6bf722831e31889e4de87ba2a0775ad9ecd35
kaswhy <sxshx818@naver.com>
2026-01-08 10:37:14 +0900

    fix(#27/auth): jwt 의존성 변경에 따른 코드 업데이트 및 로그인에 필요없는 메서드 삭제

--- src/main/java/org/sopt/snappinserver/domain/auth/jwt/JwtProvider.java
+++ src/main/java/org/sopt/snappinserver/domain/auth/jwt/JwtProvider.java
@@ -1,17 +1,11 @@
 package org.sopt.snappinserver.domain.auth.jwt;
 
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.ExpiredJwtException;
-import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import java.security.Key;
 import java.time.Instant;
 import java.util.Date;
-import org.sopt.snappinserver.domain.auth.domain.exception.AuthErrorCode;
-import org.sopt.snappinserver.domain.auth.domain.exception.AuthException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -43,31 +37,9 @@ public class JwtProvider {
         return Jwts.builder()
             .claim(CLAIM_USER_ID, userInfo.userId())
             .claim(CLAIM_ROLE, userInfo.role())
-            .setIssuedAt(Date.from(now))
-            .setExpiration(Date.from(now.plusSeconds(expireSeconds)))
-            .signWith(key, SignatureAlgorithm.HS256)
+            .issuedAt(Date.from(now))
+            .expiration(Date.from(now.plusSeconds(expireSeconds)))
+            .signWith(key)
             .compact();
-    }
-
-    public Claims parseClaims(String token) {
-        try {
-            return Jwts.parserBuilder()
-                .setSigningKey(key)
-                .build()
-                .parseClaimsJws(token)
-                .getBody();
-        } catch (ExpiredJwtException e) {
-            return e.getClaims();
-        } catch (JwtException | IllegalArgumentException e) {
-            throw new AuthException(AuthErrorCode.INVALID_JWT_TOKEN);
-        }
-    }
-
-    public Long getUserId(String token) {
-        return parseClaims(token).get(CLAIM_USER_ID, Long.class);
-    }
-
-    public void validateToken(String token) {
-        parseClaims(token);
     }
 }
