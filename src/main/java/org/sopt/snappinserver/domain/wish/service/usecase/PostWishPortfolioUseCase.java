d23999f25ba68918790d2b96194d489c6dea3be8
earl9rey <earl9rey@naver.com>
2026-01-09 22:17:07 +0900

    refactor(#48/wish): 좋아요/취소 useCase 네이밍 변경

--- src/main/java/org/sopt/snappinserver/domain/wish/service/usecase/PostWishPortfolioUseCase.java
+++ src/main/java/org/sopt/snappinserver/domain/wish/service/usecase/PostWishPortfolioUseCase.java
@@ -3,5 +3,5 @@ package org.sopt.snappinserver.domain.wish.service.usecase;
 import org.sopt.snappinserver.domain.wish.service.dto.response.WishPortfolioResult;
 
 public interface PostWishPortfolioUseCase {
-    WishPortfolioResult execute(Long userId, Long portfolioId);
+    WishPortfolioResult togglePortfolioWish(Long userId, Long portfolioId);
 }
