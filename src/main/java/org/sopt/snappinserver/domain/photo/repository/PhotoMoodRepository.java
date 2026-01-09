fc22ffdd7019814c4bff563a5cabec2da4876259
kaswhy <sxshx818@naver.com>
2026-01-09 18:53:12 +0900

    feat(#46/photo): photo 관련 리포지토리 추가

--- src/main/java/org/sopt/snappinserver/domain/photo/repository/PhotoMoodRepository.java
+++ src/main/java/org/sopt/snappinserver/domain/photo/repository/PhotoMoodRepository.java
@@ -1,10 +1,10 @@
 package org.sopt.snappinserver.domain.photo.repository;
 
-import org.sopt.snappinserver.domain.photo.domain.entity.Photo;
+import org.sopt.snappinserver.domain.photo.domain.entity.PhotoMood;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PhotoRepository extends JpaRepository<Photo, Long> {
+public interface PhotoMoodRepository extends JpaRepository<PhotoMood, Long> {
 
 }
