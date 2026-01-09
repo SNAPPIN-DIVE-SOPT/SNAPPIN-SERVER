ce5957a8fd4d224e6fb6bc38960ffd2fd2bf7088
earl9rey <earl9rey@naver.com>
2026-01-09 16:01:44 +0900

    feat(#41/product): 상품 날짜별 예약 가능 여부 조회 API 명세 추가

--- src/main/java/org/sopt/snappinserver/api/product/controller/ProductApi.java
+++ src/main/java/org/sopt/snappinserver/api/product/controller/ProductApi.java
@@ -3,6 +3,7 @@ package org.sopt.snappinserver.api.product.controller;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.sopt.snappinserver.api.product.dto.response.ProductClosedDatesResponse;
 import org.sopt.snappinserver.api.product.dto.response.ProductPeopleRangeResponse;
 import org.sopt.snappinserver.api.product.dto.response.ProductReviewsResponse;
 import org.sopt.snappinserver.api.product.dto.response.ProductReviewsMetaResponse;
@@ -38,6 +39,21 @@ public interface ProductApi {
         @PathVariable
         @Schema(description = "상품 아이디", example = "1")
         Long productId
+    );
+
+    @Operation(
+        summary = "날짜별 예약 가능 여부 조회",
+        description = "상품 예약 과정에서 달별 휴무일을 조회합니다."
+    )
+    @GetMapping("/{productId}/available/dates")
+    ApiResponseBody<ProductClosedDatesResponse, Void> getProductClosedDates(
+        @PathVariable
+        @Schema(description = "상품 아이디", example = "1")
+        Long productId,
+
+        @RequestParam
+        @Schema(description = "조회할 연/월 (yyyy-MM)", example = "2026-03", required = true)
+        String date
     );
 
 }
