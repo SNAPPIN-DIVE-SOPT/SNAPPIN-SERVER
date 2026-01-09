9fa613bc0d8108ac167e1afcadf38574a6c38354
kaswhy <sxshx818@naver.com>
2026-01-10 05:01:19 +0900

    feat(#60/curation): 무드 큐레이션 질문 조회 관련 컨트롤러 단 코드 구현

--- src/main/java/org/sopt/snappinserver/api/curation/controller/CurationController.java
+++ src/main/java/org/sopt/snappinserver/api/curation/controller/CurationController.java
@@ -1,7 +1,16 @@
 package org.sopt.snappinserver.api.curation.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.sopt.snappinserver.api.curation.code.CurationSuccessCode;
+import org.sopt.snappinserver.api.curation.dto.response.GetCurationQuestionPhotosResponse;
+import org.sopt.snappinserver.domain.auth.infra.jwt.CustomUserInfo;
+import org.sopt.snappinserver.domain.curation.service.dto.response.GetCurationQuestionResult;
+import org.sopt.snappinserver.domain.curation.service.usecase.GetCurationQuestionUseCase;
+import org.sopt.snappinserver.global.response.dto.ApiResponseBody;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/api/v1/curation")
@@ -9,4 +18,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CurationController implements CurationApi {
 
+    private final GetCurationQuestionUseCase getCurationQuestionUseCase;
+
+    @Override
+    @GetMapping
+    public ApiResponseBody<GetCurationQuestionPhotosResponse, Void> getCurationQuestion(
+        @AuthenticationPrincipal CustomUserInfo userInfo,
+        @RequestParam(required = false) Integer step
+    ) {
+        GetCurationQuestionResult result = getCurationQuestionUseCase.getCurationQuestionPhotos(
+            userInfo.userId(),
+            step
+        );
+        GetCurationQuestionPhotosResponse response = GetCurationQuestionPhotosResponse.from(result);
+
+        return ApiResponseBody.ok(CurationSuccessCode.GET_CURATION_QUESTION_SUCCESS, response);
+    }
 }
