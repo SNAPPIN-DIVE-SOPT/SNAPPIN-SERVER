08f585bbb8bcc0a515fb8ef72098ecb40a7e4c88
earl9rey <earl9rey@naver.com>
2026-01-10 03:14:57 +0900

    feat(#42/photographer): 작가/요일에 따른 작가 스케줄 조회 레포지토리 메서드 추가

--- src/main/java/org/sopt/snappinserver/domain/photographer/repository/PhotographerScheduleRepository.java
+++ src/main/java/org/sopt/snappinserver/domain/photographer/repository/PhotographerScheduleRepository.java
@@ -1,13 +1,20 @@
 package org.sopt.snappinserver.domain.photographer.repository;
 
 import java.util.List;
+import java.util.Optional;
 import org.sopt.snappinserver.domain.photographer.domain.entity.Photographer;
 import org.sopt.snappinserver.domain.photographer.domain.entity.PhotographerSchedule;
+import org.sopt.snappinserver.global.enums.WeekDay;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PhotographerScheduleRepository extends JpaRepository<PhotographerSchedule, Long> {
 
     List<PhotographerSchedule> findAllByPhotographerAndDayOffTrue(
         Photographer photographer
+    );
+
+    Optional<PhotographerSchedule> findByPhotographerAndWeekDay(
+        Photographer photographer,
+        WeekDay weekDay
     );
 }
