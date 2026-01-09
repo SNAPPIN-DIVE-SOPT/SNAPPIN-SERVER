34e015aac57f90dcdc0288a0102622a503768731
kaswhy <sxshx818@naver.com>
2026-01-09 22:02:24 +0900

    fix(#50/photo): 사진-무드 연결 api 인증 제외

--- src/main/java/org/sopt/snappinserver/global/config/security/SecurityConfig.java
+++ src/main/java/org/sopt/snappinserver/global/config/security/SecurityConfig.java
@@ -65,6 +65,7 @@ public class SecurityConfig {
                 .requestMatchers(AUTHENTICATED_URLS).authenticated()
                 .requestMatchers(HttpMethod.POST, "/api/v1/auth/login/kakao").permitAll()
                 .requestMatchers(HttpMethod.POST, "/api/v1/auth/reissue").permitAll()
+                .requestMatchers(HttpMethod.POST, "/api/v1/photos/process").permitAll()
                 .requestMatchers(HttpMethod.GET, PUBLIC_GET_URLS).permitAll()
                 .anyRequest().authenticated()
             )
