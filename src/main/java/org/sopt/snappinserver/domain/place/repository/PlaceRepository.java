e902bd2b99494feb8f1106a69493f87e0a1b9938
earl9rey <earl9rey@naver.com>
2026-01-10 18:30:37 +0900

    refactor(#71/product): 장소 레포지토리 findByName 메서드 삭제

--- src/main/java/org/sopt/snappinserver/domain/place/repository/PlaceRepository.java
+++ src/main/java/org/sopt/snappinserver/domain/place/repository/PlaceRepository.java
@@ -1,10 +1,8 @@
 package org.sopt.snappinserver.domain.place.repository;
 
-import java.util.Optional;
 import org.sopt.snappinserver.domain.place.domain.entity.Place;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PlaceRepository extends JpaRepository<Place, Long> {
 
-    Optional<Place> findByName(String name);
 }
