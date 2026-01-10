9d870c0b15cd7d45c53291c349ca80a1863559b5
earl9rey <earl9rey@naver.com>
2026-01-10 22:21:20 +0900

    feat(#68/wish): 위시 포트폴리오 목록 도메인 결과 DTO 추가

---
+++ src/main/java/org/sopt/snappinserver/domain/wish/service/dto/response/WishedPortfoliosResult.java
@@ -0,0 +1,12 @@
+package org.sopt.snappinserver.domain.wish.service.dto.response;
+
+import java.util.List;
+
+public record WishedPortfoliosResult(
+    List<WishedPortfolioResult> portfolios
+) {
+
+    public static WishedPortfoliosResult from(List<WishedPortfolioResult> portfolios) {
+        return new WishedPortfoliosResult(portfolios);
+    }
+}
