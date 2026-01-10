4e4dd5800ce075449b3c777e392037cb5246a883
kaswhy <sxshx818@naver.com>
2026-01-10 15:43:39 +0900

    feat(#60/global): global Exception Handler에서 컨트롤러단 @Valid 검증 후 자체 예외 반환 가능하도록 수정

--- src/main/java/org/sopt/snappinserver/global/exception/GlobalExceptionHandler.java
+++ src/main/java/org/sopt/snappinserver/global/exception/GlobalExceptionHandler.java
@@ -1,17 +1,18 @@
 package org.sopt.snappinserver.global.exception;
 
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
 import java.time.Instant;
 import java.time.format.DateTimeParseException;
-import java.util.Objects;
 import lombok.extern.slf4j.Slf4j;
 import org.sopt.snappinserver.global.response.dto.ApiResponseBody;
 import org.sopt.snappinserver.global.response.dto.ErrorMeta;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.resource.NoResourceFoundException;
@@ -54,14 +55,17 @@ public class GlobalExceptionHandler {
             .body(ApiResponseBody.onFailure(CommonErrorCode.RESOURCE_NOT_FOUND, meta));
     }
 
-    @ExceptionHandler(MissingServletRequestParameterException.class)
-    public ResponseEntity<ApiResponseBody<Void, ErrorMeta>> handleMissingServletRequestParameterException(
-        MissingServletRequestParameterException exception,
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<ApiResponseBody<Void, ErrorMeta>> handleConstraintViolation(
+        ConstraintViolationException exception,
         HttpServletRequest request
     ) {
-        log.error("MissingServletRequestParameterException: {}", exception.getMessage());
+        log.error("ConstraintViolationException: {}", exception.getMessage());
 
-        String message = exception.getParameterName() + "는 필수입니다.";
+        String message = exception.getConstraintViolations().stream()
+            .findFirst()
+            .map(ConstraintViolation::getMessage)
+            .orElse("잘못된 요청입니다.");
 
         ErrorMeta meta = new ErrorMeta(
             request.getRequestURI(),
@@ -71,7 +75,7 @@ public class GlobalExceptionHandler {
         return ResponseEntity
             .status(HttpStatus.BAD_REQUEST)
             .body(ApiResponseBody.onFailure(
-                    CommonErrorCode.INVALID_MAPPING_PARAMETER,
+                    CommonErrorCode.INVALID_REQUEST_VARIABLE,
                     message,
                     meta
                 )
@@ -79,15 +83,18 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ApiResponseBody<Void, ErrorMeta>> handleMissingServletRequestParameterException(
+    public ResponseEntity<ApiResponseBody<Void, ErrorMeta>> handleMethodArgumentNotValidException(
         MethodArgumentNotValidException exception,
         HttpServletRequest request
     ) {
         log.error("MethodArgumentNotValidException: {}", exception.getMessage());
 
-        String message = Objects.requireNonNull(exception.getBindingResult()
-                .getFieldError())
-            .getDefaultMessage();
+        String message = exception.getBindingResult()
+            .getFieldErrors()
+            .stream()
+            .findFirst()
+            .map(FieldError::getDefaultMessage)
+            .orElse("잘못된 요청입니다.");
 
         ErrorMeta meta = new ErrorMeta(
             request.getRequestURI(),
