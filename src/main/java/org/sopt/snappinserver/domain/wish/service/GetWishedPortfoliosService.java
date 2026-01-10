c6297389067ea010c59456b75ec8f4ec9eae7408
earl9rey <earl9rey@naver.com>
2026-01-11 00:29:37 +0900

    refactor(#68/wish): 위시 포트폴리오 조회 서비스 로직 분리 및 가독성 개선

--- src/main/java/org/sopt/snappinserver/domain/wish/service/GetWishedPortfoliosService.java
+++ src/main/java/org/sopt/snappinserver/domain/wish/service/GetWishedPortfoliosService.java
@@ -2,8 +2,13 @@ package org.sopt.snappinserver.domain.wish.service;
 
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.sopt.snappinserver.domain.photo.domain.entity.Photo;
 import org.sopt.snappinserver.domain.portfolio.domain.entity.Portfolio;
+import org.sopt.snappinserver.domain.portfolio.domain.entity.PortfolioPhoto;
+import org.sopt.snappinserver.domain.portfolio.repository.PortfolioPhotoRepository;
 import org.sopt.snappinserver.domain.user.domain.entity.User;
+import org.sopt.snappinserver.domain.user.domain.exception.UserErrorCode;
+import org.sopt.snappinserver.domain.user.domain.exception.UserException;
 import org.sopt.snappinserver.domain.user.repository.UserRepository;
 import org.sopt.snappinserver.domain.wish.domain.entity.WishPortfolio;
 import org.sopt.snappinserver.domain.wish.repository.WishPortfolioRepository;
@@ -17,20 +22,37 @@ import org.springframework.stereotype.Service;
 public class GetWishedPortfoliosService implements GetWishedPortfoliosUseCase {
 
     private final WishPortfolioRepository wishPortfolioRepository;
+    private final PortfolioPhotoRepository portfolioPhotoRepository;
     private final UserRepository userRepository;
 
     @Override
     public WishedPortfoliosResult getWishedPortfolios(Long userId) {
-        User user = userRepository.getById(userId);
-
-        List<WishedPortfolioResult> results =
-            wishPortfolioRepository.findAllByUser(user)
-                .stream()
-                .map(WishPortfolio::getPortfolio)
-                .map(WishedPortfolioResult::from)
-                .toList();
+        User user = getUser(userId);
+        List<WishedPortfolioResult> results = getWishedPortfolioResults(user);
 
         return WishedPortfoliosResult.from(results);
     }
-}
 
+    private User getUser(Long userId) {
+        return userRepository.findById(userId)
+            .orElseThrow(() -> new UserException(UserErrorCode.USER_NOT_FOUND));
+    }
+
+    private List<WishedPortfolioResult> getWishedPortfolioResults(User user) {
+        return wishPortfolioRepository
+            .findAllByUser(user)
+            .stream()
+            .map(WishPortfolio::getPortfolio)
+            .map(this::toWishedPortfolioResult).toList();
+    }
+
+    private WishedPortfolioResult toWishedPortfolioResult(Portfolio portfolio) {
+        String imageUrl = portfolioPhotoRepository
+            .findFirstByPortfolioOrderByDisplayOrderAsc(portfolio)
+            .map(PortfolioPhoto::getPhoto)
+            .map(Photo::getImageUrl)
+            .orElse(null);
+
+        return WishedPortfolioResult.of(portfolio.getId(), imageUrl);
+    }
+}
