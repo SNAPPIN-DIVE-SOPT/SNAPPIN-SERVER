b9b64517326fad655b53002494bbcf136000fabd
earl9rey <earl9rey@naver.com>
2026-01-10 17:30:29 +0900

    feat(#71/place): 장소 레포지토리 메서드 추가

--- src/main/java/org/sopt/snappinserver/domain/place/repository/PlaceRepository.java
+++ src/main/java/org/sopt/snappinserver/domain/place/repository/PlaceRepository.java
@@ -1,8 +1,10 @@
 package org.sopt.snappinserver.domain.place.repository;
 
+import java.util.Optional;
 import org.sopt.snappinserver.domain.place.domain.entity.Place;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PlaceRepository extends JpaRepository<Place, Long> {
 
+    Optional<Place> findByName(String name);
 }
