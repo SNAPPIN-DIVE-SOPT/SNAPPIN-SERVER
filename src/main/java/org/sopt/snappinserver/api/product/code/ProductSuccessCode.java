94232902eeac5ca4d4dad9c6826884b348f1221d
earl9rey <earl9rey@naver.com>
2026-01-10 18:11:23 +0900

    refactor(#71/product): 예약 성공 코드 201로 변경

--- src/main/java/org/sopt/snappinserver/api/product/code/ProductSuccessCode.java
+++ src/main/java/org/sopt/snappinserver/api/product/code/ProductSuccessCode.java
@@ -12,7 +12,7 @@ public enum ProductSuccessCode implements SuccessCode {
     GET_PRODUCT_PEOPLE_RANGE_OK(200, "PRODUCT_200_002", "상품의 촬영 가능 인원 수 조회에 성공했습니다."),
     GET_PRODUCT_AVAILABLE_DATE_OK(200, "PRODUCT_200_003", "상품의 날짜별 예약 가능 여부 조회에 성공했습니다."),
     GET_PRODUCT_AVAILABLE_TIMES_OK(200, "PRODUCT_200_004", "상품의 시간대별 예약 가능 여부 조회에 성공했습니다."),
-    POST_PRODUCT_RESERVATION_OK(200, "PRODUCT_200_005", "상품 예약 요청에 성공했습니다.");
+    POST_PRODUCT_RESERVATION_OK(201, "PRODUCT_200_005", "상품 예약 생성에 성공했습니다.");
 
     private final int status;
     private final String code;
