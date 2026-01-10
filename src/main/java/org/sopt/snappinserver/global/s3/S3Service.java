7db9dd8ecc44b9fda89a1028c6da47b3844c1b5f
kaswhy <sxshx818@naver.com>
2026-01-10 14:36:16 +0900

    refactor(#60/s3): s3 키 존재 여부 검증 로직 추가

--- src/main/java/org/sopt/snappinserver/global/s3/S3Service.java
+++ src/main/java/org/sopt/snappinserver/global/s3/S3Service.java
@@ -5,8 +5,8 @@ import java.time.Duration;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 import software.amazon.awssdk.services.s3.presigner.model.PresignedGetObjectRequest;
 
 @Service
@@ -23,6 +23,7 @@ public class S3Service {
 
     public String getPresignedUrl(String storedFileName) {
         try {
+            validateStoredFileNameExsits(storedFileName);
             GetObjectRequest getObjectRequest = GetObjectRequest.builder()
                 .bucket(bucketName)
                 .key(storedFileName)
@@ -37,6 +38,12 @@ public class S3Service {
             return presignedRequest.url().toString();
         } catch (Exception e) {
             throw new S3Exception(S3ErrorCode.S3_SERVICE_UNAVAILABLE);
+        }
+    }
+
+    private static void validateStoredFileNameExsits(String storedFileName) {
+        if(storedFileName == null || storedFileName.isBlank()) {
+            throw new S3Exception(S3ErrorCode.S3_KEY_REQUIRED);
         }
     }
 }
