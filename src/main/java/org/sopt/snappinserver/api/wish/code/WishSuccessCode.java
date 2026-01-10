e2e6a5190fa73a4f85758a6a4d8797956d6c6ba4
earl9rey <earl9rey@naver.com>
2026-01-10 14:19:15 +0900

    feat(#42/wish): 상품 좋아요 성공 코드 추가

--- src/main/java/org/sopt/snappinserver/api/wish/code/WishSuccessCode.java
+++ src/main/java/org/sopt/snappinserver/api/wish/code/WishSuccessCode.java
@@ -9,7 +9,9 @@ import org.sopt.snappinserver.global.response.code.common.SuccessCode;
 public enum WishSuccessCode implements SuccessCode {
 
     POST_WISH_LIKE_PORTFOLIO_OK(200, "WISH_200_001", "포트폴리오 좋아요 처리에 성공했습니다."),
-    POST_WISH_CANCEL_PORTFOLIO_OK(200, "WISH_200_002", "포트폴리오 좋아요 취소에 성공했습니다.");
+    POST_WISH_CANCEL_PORTFOLIO_OK(200, "WISH_200_002", "포트폴리오 좋아요 취소에 성공했습니다."),
+    POST_WISH_LIKE_PRODUCT_OK(200, "WISH_200_003", "상품 좋아요 처리에 성공했습니다."),
+    POST_WISH_CANCEL_PRODUCT_OK(200, "WISH_200_004", "상품 좋아요 취소에 성공했습니다.");
 
 
     private final int status;
