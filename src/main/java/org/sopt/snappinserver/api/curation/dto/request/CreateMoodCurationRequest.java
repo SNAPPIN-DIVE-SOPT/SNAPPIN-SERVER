e118ec0e2bff64662473bda6afd40907dc9bbe82
kaswhy <sxshx818@naver.com>
2026-01-11 11:50:38 +0900

    refactor(#77/curation): photoIds NotNull 검증 추가

--- src/main/java/org/sopt/snappinserver/api/curation/dto/request/CreateMoodCurationRequest.java
+++ src/main/java/org/sopt/snappinserver/api/curation/dto/request/CreateMoodCurationRequest.java
@@ -1,6 +1,7 @@
 package org.sopt.snappinserver.api.curation.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import java.util.List;
 
@@ -8,6 +9,7 @@ import java.util.List;
 public record CreateMoodCurationRequest(
 
     @Schema(description = "사용자가 선택한 사진 ID 목록", example = "[1, 2, 3, 4, 5]")
+    @NotNull(message = "사진 ID 목록은 비어있을 수 없습니다.")
     @Size(min = 5, message = "사용자가 선택한 사진 개수는 5개여야 합니다.")
     @Size(max = 5, message = "사용자가 선택한 사진 개수는 5개여야 합니다.")
     List<Long> photoIds
