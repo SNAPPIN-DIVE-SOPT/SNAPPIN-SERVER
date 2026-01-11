d71040dcf8edbd92487db244f2c168b3e7f3dd10
kaswhy <sxshx818@naver.com>
2026-01-12 03:19:50 +0900

    feat(#64/users): 작가 조회 리포지토리 코드 구현

--- src/main/java/org/sopt/snappinserver/domain/photographer/repository/PhotographerRepository.java
+++ src/main/java/org/sopt/snappinserver/domain/photographer/repository/PhotographerRepository.java
@@ -1,8 +1,12 @@
 package org.sopt.snappinserver.domain.photographer.repository;
 
+import java.util.Optional;
 import org.sopt.snappinserver.domain.photographer.domain.entity.Photographer;
+import org.sopt.snappinserver.domain.user.domain.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PhotographerRepository extends JpaRepository<Photographer, Long> {
+
+    Optional<Photographer> findByUser(User user);
 
 }
