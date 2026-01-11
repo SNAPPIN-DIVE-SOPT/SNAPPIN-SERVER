44737285bfedaefc463ef25466bc40b075ad66f6
earl9rey <earl9rey@naver.com>
2026-01-11 20:35:05 +0900

    feat(#72/reservation): 예약 리뷰 등록 API 인터페이스 구현

--- src/main/java/org/sopt/snappinserver/api/reservation/controller/ReservationApi.java
+++ src/main/java/org/sopt/snappinserver/api/reservation/controller/ReservationApi.java
@@ -1,8 +1,33 @@
 package org.sopt.snappinserver.api.reservation.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import org.sopt.snappinserver.api.reservation.dto.request.CreateReservationReviewRequest;
+import org.sopt.snappinserver.api.reservation.dto.response.CreateReservationReviewResponse;
+import org.sopt.snappinserver.domain.auth.infra.jwt.CustomUserInfo;
+import org.sopt.snappinserver.global.response.dto.ApiResponseBody;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Tag(name = "010 - Reservation", description = "예약 관련 API")
 public interface ReservationApi {
+    @Operation(
+        summary = "리뷰 등록",
+        description = "촬영 완료된 예약 상품에 대해 리뷰를 작성합니다."
+    )
+    @PostMapping("/{reservationId}/reviews")
+    ApiResponseBody<CreateReservationReviewResponse, Void> createReview(
+
+        @Parameter(hidden = true)
+        CustomUserInfo userInfo,
+
+        @PathVariable Long reservationId,
+
+        @Valid @RequestBody CreateReservationReviewRequest request
+    );
 
 }
