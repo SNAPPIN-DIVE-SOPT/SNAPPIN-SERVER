b66c653a3922d3ee9dcfefe362ad18bf16b68b44
earl9rey <earl9rey@naver.com>
2026-01-11 20:40:39 +0900

    feat(#72/reservation): 예약 리뷰 등록 관련 성공/예외 코드 추가

---
+++ src/main/java/org/sopt/snappinserver/api/reservation/code/ReservationSuccessCode.java
@@ -0,0 +1,19 @@
+package org.sopt.snappinserver.api.reservation.code;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.sopt.snappinserver.global.response.code.common.SuccessCode;
+
+@Getter
+@RequiredArgsConstructor
+public enum ReservationSuccessCode implements SuccessCode {
+
+    // 200 OK
+
+    // 201 CREATED
+    POST_RESERVATION_REVIEW_CREATED(201, "RESERVATION_201_001", "상품 리뷰 등록에 성공했습니다.");
+
+    private final int status;
+    private final String code;
+    private final String message;
+}
