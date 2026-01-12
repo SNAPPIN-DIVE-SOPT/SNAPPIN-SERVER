ab77e5e4a5c5599d50cac302af7439b38320aeca
kaswhy <sxshx818@naver.com>
2026-01-12 22:31:00 +0900

    feat(#87/portfolio): 인기 무드 포트폴리오 조회 서비스 코드 구현

---
+++ src/main/java/org/sopt/snappinserver/domain/portfolio/service/dto/response/GetPopularPortfolioResult.java
@@ -0,0 +1,38 @@
+package org.sopt.snappinserver.domain.portfolio.service.dto.response;
+
+import java.util.Comparator;
+import java.util.List;
+import org.sopt.snappinserver.domain.mood.domain.entity.Mood;
+import org.sopt.snappinserver.domain.portfolio.domain.entity.Portfolio;
+import org.sopt.snappinserver.domain.portfolio.domain.entity.PortfolioPhoto;
+
+public record GetPopularPortfolioResult(
+    Long id,
+    List<GetImageResult> images,
+    List<String> moods,
+    String photographerName
+) {
+
+    public static GetPopularPortfolioResult of(
+        Portfolio portfolio,
+        List<PortfolioPhoto> photos,
+        List<Mood> moods
+    ) {
+        return new GetPopularPortfolioResult(
+            portfolio.getId(),
+
+            photos.stream()
+                .sorted(Comparator.comparingInt(PortfolioPhoto::getDisplayOrder))
+                .map(GetImageResult::from)
+                .toList(),
+
+            moods.stream()
+                .map(Mood::getName)
+                .toList(),
+
+            portfolio.getProduct().getPhotographer().getName()
+        );
+    }
+}
+
+
---
+++ src/main/java/org/sopt/snappinserver/domain/portfolio/service/usecase/GetPopularPortfolioListUseCase.java
@@ -0,0 +1,8 @@
+package org.sopt.snappinserver.domain.portfolio.service.usecase;
+
+import org.sopt.snappinserver.domain.portfolio.service.dto.response.GetPopularPortfolioListResult;
+
+public interface GetPopularPortfolioListUseCase {
+
+    GetPopularPortfolioListResult getPopularPortfolioList();
+}
