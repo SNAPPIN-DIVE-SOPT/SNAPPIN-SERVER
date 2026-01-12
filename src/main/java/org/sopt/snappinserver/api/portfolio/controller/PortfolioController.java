b813cd417bb47fc7fe67bd4fb470a66c67668da7
kaswhy <sxshx818@naver.com>
2026-01-12 22:31:15 +0900

    feat(#87/portfolio): 인기 무드 포트폴리오 조회 컨트롤러 코드 구현

--- src/main/java/org/sopt/snappinserver/api/portfolio/controller/PortfolioController.java
+++ src/main/java/org/sopt/snappinserver/api/portfolio/controller/PortfolioController.java
@@ -1,6 +1,13 @@
 package org.sopt.snappinserver.api.portfolio.controller;
 
+import static org.sopt.snappinserver.api.portfolio.code.PortfolioSuccessCode.GET_POPULAR_PORTFOLIOS_OK;
+
 import lombok.RequiredArgsConstructor;
+import org.sopt.snappinserver.api.portfolio.dto.response.GetPopularPortfolioListResponse;
+import org.sopt.snappinserver.api.portfolio.dto.response.GetPopularPortfolioResponse;
+import org.sopt.snappinserver.domain.portfolio.service.dto.response.GetPopularPortfolioListResult;
+import org.sopt.snappinserver.domain.portfolio.service.usecase.GetPopularPortfolioListUseCase;
+import org.sopt.snappinserver.global.response.dto.ApiResponseBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,4 +16,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class PortfolioController {
 
+    private final GetPopularPortfolioListUseCase getPopularPortfolioListUseCase;
+
+    public ApiResponseBody<GetPopularPortfolioListResponse, Void> getPopularPortfolios() {
+        GetPopularPortfolioListResult result = getPopularPortfolioListUseCase
+            .getPopularPortfolioList();
+        GetPopularPortfolioListResponse response = GetPopularPortfolioListResponse.from(result);
+
+        return ApiResponseBody.ok(GET_POPULAR_PORTFOLIOS_OK, response);
+    }
 }
---
+++ src/main/java/org/sopt/snappinserver/api/portfolio/dto/response/GetPopularPortfolioResponse.java
@@ -0,0 +1,33 @@
+package org.sopt.snappinserver.api.portfolio.dto.response;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import java.util.List;
+import org.sopt.snappinserver.domain.portfolio.service.dto.response.GetPopularPortfolioResult;
+
+@Schema(description = "포트폴리오 응답 DTO")
+public record GetPopularPortfolioResponse(
+
+    @Schema(description = "포트폴리오 ID")
+    Long id,
+
+    @Schema(description = "포트폴리오 이미지 목록")
+    List<GetImageResponse> images,
+
+    @Schema(description = "포트폴리오 무드 목록")
+    List<String> moods,
+
+    @Schema(description = "포트폴리오 작가명")
+    String photographerName
+) {
+
+    public static GetPopularPortfolioResponse from(GetPopularPortfolioResult result) {
+        return new GetPopularPortfolioResponse(
+            result.id(),
+            result.images().stream()
+                .map(GetImageResponse::from)
+                .toList(),
+            result.moods(),
+            result.photographerName()
+        );
+    }
+}
