c70656bff19959018f1589c0df70b1ce976e46a1
kaswhy <sxshx818@naver.com>
2026-01-12 22:30:45 +0900

    feat(#87/portfolio): 인기 무드 포트폴리오 조회 리포지토리 코드 구현

---
+++ src/main/java/org/sopt/snappinserver/domain/portfolio/repository/PortfolioMoodRepository.java
@@ -0,0 +1,22 @@
+package org.sopt.snappinserver.domain.portfolio.repository;
+
+import java.util.List;
+import org.sopt.snappinserver.domain.portfolio.domain.entity.PortfolioMood;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface PortfolioMoodRepository extends JpaRepository<PortfolioMood, Long> {
+
+    @Query("""
+            SELECT pm
+            FROM PortfolioMood pm
+            JOIN FETCH pm.mood
+            WHERE pm.portfolio.id IN :portfolioIds
+        """)
+
+    List<PortfolioMood> findByPortfolioIds(
+        @Param("portfolioIds") List<Long> portfolioIds
+    );
+
+}
