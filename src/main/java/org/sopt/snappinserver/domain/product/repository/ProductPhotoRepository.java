32c7fd80777b400eebc38e8534729f7e1422e642
earl9rey <earl9rey@naver.com>
2026-01-12 14:13:38 +0900

    fix(#69/product): ProductPhoto 레포지토리 JpaRepository 상속 처리

--- src/main/java/org/sopt/snappinserver/domain/product/repository/ProductPhotoRepository.java
+++ src/main/java/org/sopt/snappinserver/domain/product/repository/ProductPhotoRepository.java
@@ -3,10 +3,11 @@ package org.sopt.snappinserver.domain.product.repository;
 import java.util.Optional;
 import org.sopt.snappinserver.domain.product.domain.entity.Product;
 import org.sopt.snappinserver.domain.product.domain.entity.ProductPhoto;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProductPhotoRepository {
+public interface ProductPhotoRepository extends JpaRepository<ProductPhoto, Long> {
 
     Optional<ProductPhoto> findFirstByProductOrderByDisplayOrderAsc(Product product);
 
