ad46e2a66261aa8fe994b76bdb835c3f1191942b
earl9rey <earl9rey@naver.com>
2026-01-14 00:15:47 +0900

    feat(#89/reservation): 예약 상세 조회 API 컨트롤러 구현

--- src/main/java/org/sopt/snappinserver/api/reservation/controller/ReservationController.java
+++ src/main/java/org/sopt/snappinserver/api/reservation/controller/ReservationController.java
@@ -4,15 +4,18 @@ import lombok.RequiredArgsConstructor;
 import org.sopt.snappinserver.api.reservation.code.ReservationSuccessCode;
 import org.sopt.snappinserver.api.reservation.dto.request.CreateReservationReviewRequest;
 import org.sopt.snappinserver.api.reservation.dto.response.CreateReservationReviewResponse;
+import org.sopt.snappinserver.api.reservation.dto.response.ReservationDetailResponse;
 import org.sopt.snappinserver.api.reservation.dto.response.ReservationListResponse;
 import org.sopt.snappinserver.domain.auth.infra.jwt.CustomUserInfo;
 import org.sopt.snappinserver.domain.reservation.domain.enums.ReservationStatusTab;
 import org.sopt.snappinserver.domain.reservation.service.dto.request.CreateReservationReviewCommand;
 import org.sopt.snappinserver.domain.reservation.service.dto.response.CreateReservationReviewResult;
+import org.sopt.snappinserver.domain.reservation.service.usecase.GetReservationDetailUseCase;
 import org.sopt.snappinserver.domain.reservation.service.usecase.GetReservationListUseCase;
 import org.sopt.snappinserver.domain.reservation.service.usecase.PostReservationReviewUseCase;
 import org.sopt.snappinserver.global.response.dto.ApiResponseBody;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,6 +26,7 @@ public class ReservationController implements ReservationApi {
 
     private final PostReservationReviewUseCase postReservationReviewUseCase;
     private final GetReservationListUseCase getReservationListUseCase;
+    private final GetReservationDetailUseCase getReservationDetailUseCase;
 
     @Override
     public ApiResponseBody<CreateReservationReviewResponse, Void> createReview(
@@ -60,5 +64,19 @@ public class ReservationController implements ReservationApi {
         );
     }
 
-
+    @Override
+    public ApiResponseBody<ReservationDetailResponse, Void> getReservationDetail(
+        @AuthenticationPrincipal CustomUserInfo userInfo,
+        Long reservationId
+    ) {
+        return ApiResponseBody.ok(
+            ReservationSuccessCode.GET_RESERVATION_DETAIL_OK,
+            ReservationDetailResponse.from(
+                getReservationDetailUseCase.getReservationDetail(
+                    userInfo.userId(),
+                    reservationId
+                )
+            )
+        );
+    }
 }
