061a219fabe6eb3ff2fdaf82ab63a9be340f9ac6
kaswhy <sxshx818@naver.com>
2026-01-13 16:49:39 +0900

    fix(#87/portfolio): Param 어노테이션 의존성 jpa로 변경

--- src/main/java/org/sopt/snappinserver/domain/mood/repository/MoodRepository.java
+++ src/main/java/org/sopt/snappinserver/domain/mood/repository/MoodRepository.java
@@ -1,11 +1,11 @@
 package org.sopt.snappinserver.domain.mood.repository;
 
-import io.lettuce.core.dynamic.annotation.Param;
 import java.util.List;
 import lombok.NonNull;
 import org.sopt.snappinserver.domain.mood.domain.entity.Mood;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface MoodRepository extends JpaRepository<Mood, Long> {
 
