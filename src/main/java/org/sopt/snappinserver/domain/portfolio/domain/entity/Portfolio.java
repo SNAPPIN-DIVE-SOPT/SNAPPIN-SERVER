90d185bc659a6907e9495c58ad1f9432efeaa933
kaswhy <sxshx818@naver.com>
2026-01-13 17:35:32 +0900

    feat(#87/portfolio): DB 레벨에 외래키 not null 제약조건 추가

--- src/main/java/org/sopt/snappinserver/domain/portfolio/domain/entity/Portfolio.java
+++ src/main/java/org/sopt/snappinserver/domain/portfolio/domain/entity/Portfolio.java
@@ -12,7 +12,6 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.SequenceGenerator;
 import java.time.LocalTime;
-import javax.sound.sampled.Port;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
