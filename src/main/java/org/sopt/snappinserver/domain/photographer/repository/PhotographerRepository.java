6bdc57bd523b7b409e885ffcdb41f91e20ea0428
kaswhy <sxshx818@naver.com>
2026-01-13 12:07:42 +0900

    feat(#65/photographer): 작가 프로필 보유 여부 조회 API

--- src/main/java/org/sopt/snappinserver/domain/photographer/repository/PhotographerRepository.java
+++ src/main/java/org/sopt/snappinserver/domain/photographer/repository/PhotographerRepository.java
@@ -8,5 +8,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface PhotographerRepository extends JpaRepository<Photographer, Long> {
 
     Optional<Photographer> findByUser(User user);
-
+    boolean existsByUser(User user);
 }
