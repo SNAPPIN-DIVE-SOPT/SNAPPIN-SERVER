677dde1fccfb038d38823c7b72d6a1cd34cdac47
earl9rey <earl9rey@naver.com>
2026-01-13 10:29:16 +0900

    feat(#88/product): 예약 목록 조회를 위한 상품 무드 배치 조회 추가

--- src/main/java/org/sopt/snappinserver/domain/product/repository/ProductMoodRepository.java
+++ src/main/java/org/sopt/snappinserver/domain/product/repository/ProductMoodRepository.java
@@ -8,4 +8,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface ProductMoodRepository extends JpaRepository<ProductMood, Long> {
 
     List<ProductMood> findAllByProduct(Product product);
+
+    List<ProductMood> findAllByProductIdIn(List<Long> productIds);
 }
