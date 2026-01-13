87a046db0d33baa3e09e5dbcd9c9b7945f5e5082
kaswhy <sxshx818@naver.com>
2026-01-13 16:50:20 +0900

    refactor(#87/portfolio): PortfolioMoodRepository에 Repository 어노테이션 추가

--- src/main/java/org/sopt/snappinserver/domain/portfolio/repository/PortfolioMoodRepository.java
+++ src/main/java/org/sopt/snappinserver/domain/portfolio/repository/PortfolioMoodRepository.java
@@ -5,7 +5,9 @@ import org.sopt.snappinserver.domain.portfolio.domain.entity.PortfolioMood;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PortfolioMoodRepository extends JpaRepository<PortfolioMood, Long> {
 
     @Query("""
