1743d95260a864571b39615e737d3a3b84ebeafe
kaswhy <sxshx818@naver.com>
2026-01-13 11:07:12 +0900

    feat(#91/place): 장소 검색 리포지토리 코드 작성

--- src/main/java/org/sopt/snappinserver/domain/place/repository/PlaceRepository.java
+++ src/main/java/org/sopt/snappinserver/domain/place/repository/PlaceRepository.java
@@ -1,8 +1,11 @@
 package org.sopt.snappinserver.domain.place.repository;
 
+
+import java.util.List;
 import org.sopt.snappinserver.domain.place.domain.entity.Place;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PlaceRepository extends JpaRepository<Place, Long> {
 
+    List<Place> findTop4ByNameContainingOrderByNameAsc(String name);
 }
