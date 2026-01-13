4a646284d154d8fe5b4ed28a0449060b3a2ca4e7
kaswhy <sxshx818@naver.com>
2026-01-13 11:07:46 +0900

    feat(#91/place): 장소 검색 컨트롤러 코드 작성

---
+++ src/main/java/org/sopt/snappinserver/api/place/dto/response/GetPlaceListResponse.java
@@ -0,0 +1,21 @@
+package org.sopt.snappinserver.api.place.dto.response;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import java.util.List;
+import org.sopt.snappinserver.domain.place.service.dto.response.GetPlaceListResult;
+
+@Schema(description = "촬영 장소 목록 검색 API")
+public record GetPlaceListResponse(
+
+    @Schema
+    List<GetPlaceResponse> places
+) {
+
+    public static GetPlaceListResponse from(GetPlaceListResult result) {
+        return new GetPlaceListResponse(
+            result.places().stream()
+                .map(GetPlaceResponse::from)
+                .toList()
+        );
+    }
+}
