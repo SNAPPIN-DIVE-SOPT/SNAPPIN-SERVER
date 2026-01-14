5da7be8dba8cdce1a7ca26024b361bdd8ecb3882
earl9rey <earl9rey@naver.com>
2026-01-14 17:31:51 +0900

    feat(#104/reservation): 예약 고객 확인 메서드 네이밍 변경

--- src/main/java/org/sopt/snappinserver/domain/reservation/service/PostReservationReviewService.java
+++ src/main/java/org/sopt/snappinserver/domain/reservation/service/PostReservationReviewService.java
@@ -61,7 +61,7 @@ public class PostReservationReviewService implements PostReservationReviewUseCas
     }
 
     private void validateReservationClient(Reservation reservation, Long userId) {
-        if (!reservation.validateReservationClient(userId)) {
+        if (!reservation.isReservationClient(userId)) {
             throw new ReservationException(ReservationErrorCode.RESERVATION_USER_NOT_MATCH);
         }
     }
