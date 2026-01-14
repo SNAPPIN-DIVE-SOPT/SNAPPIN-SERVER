b7d100bf29526c48e28ad4d41bbc99ff2b3f71d3
earl9rey <earl9rey@naver.com>
2026-01-14 23:41:13 +0900

    feat(#106/reservation): 촬영 완료 관련 성공/에러 코드 추가

--- src/main/java/org/sopt/snappinserver/api/reservation/code/ReservationSuccessCode.java
+++ src/main/java/org/sopt/snappinserver/api/reservation/code/ReservationSuccessCode.java
@@ -29,6 +29,11 @@ public enum ReservationSuccessCode implements SuccessCode {
         "RESERVATION_200_004",
         "예약 취소에 성공했습니다."
     ),
+    PATCH_RESERVATION_COMPLETE_OK(
+        200,
+        "RESERVATION_200_005",
+        "촬영 완료 처리에 성공했습니다."
+    ),
 
     // 201 CREATED
     POST_RESERVATION_REVIEW_CREATED(201, "RESERVATION_201_001", "예약 리뷰 등록에 성공했습니다.");
