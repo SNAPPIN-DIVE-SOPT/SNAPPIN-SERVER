269b36ca56378b360c2a1958224efc68f34b15c3
earl9rey <earl9rey@naver.com>
2026-01-14 17:33:23 +0900

    feat(#104/reservation): 예약 결제하기 API 인터페이스 구현

--- src/main/java/org/sopt/snappinserver/api/reservation/controller/ReservationApi.java
+++ src/main/java/org/sopt/snappinserver/api/reservation/controller/ReservationApi.java
@@ -8,12 +8,14 @@ import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import org.sopt.snappinserver.api.reservation.dto.request.CreateReservationReviewRequest;
 import org.sopt.snappinserver.api.reservation.dto.response.CreateReservationReviewResponse;
+import org.sopt.snappinserver.api.reservation.dto.response.PayReservationResponse;
 import org.sopt.snappinserver.api.reservation.dto.response.ReservationDetailResponse;
 import org.sopt.snappinserver.api.reservation.dto.response.ReservationListResponse;
 import org.sopt.snappinserver.domain.auth.infra.jwt.CustomUserInfo;
 import org.sopt.snappinserver.domain.reservation.domain.enums.ReservationStatusTab;
 import org.sopt.snappinserver.global.response.dto.ApiResponseBody;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -58,6 +60,20 @@ public interface ReservationApi {
     )
     @GetMapping("/{reservationId}")
     ApiResponseBody<ReservationDetailResponse, Void> getReservationDetail(
+
+        @Parameter(hidden = true)
+        CustomUserInfo userInfo,
+
+        @Schema(description = "예약 아이디", example = "1")
+        @PathVariable @NotNull Long reservationId
+    );
+
+    @Operation(
+        summary = "결제하기",
+        description = "고객에게 결제 요청된 예약에 대해 결제 완료 상태로 변경합니다."
+    )
+    @PatchMapping("/{reservationId}/pay")
+    ApiResponseBody<PayReservationResponse, Void> patchReservationPay(
 
         @Parameter(hidden = true)
         CustomUserInfo userInfo,
