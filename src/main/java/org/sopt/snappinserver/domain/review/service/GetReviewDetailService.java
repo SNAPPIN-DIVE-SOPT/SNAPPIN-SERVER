3ff5992d4f1b7f2938ef720ed3f9d3bb786eafa6
earl9rey <earl9rey@naver.com>
2026-01-16 03:45:24 +0900

    feat(#134/review): 리뷰 상세 조회 비즈니스 로직 구현

---
+++ src/main/java/org/sopt/snappinserver/domain/review/service/GetReviewDetailService.java
@@ -0,0 +1,46 @@
+package org.sopt.snappinserver.domain.review.service;
+
+import java.util.List;
+import java.time.ZoneId;
+import lombok.RequiredArgsConstructor;
+import org.sopt.snappinserver.domain.review.domain.entity.Review;
+import org.sopt.snappinserver.domain.review.domain.exception.ReviewErrorCode;
+import org.sopt.snappinserver.domain.review.domain.exception.ReviewException;
+import org.sopt.snappinserver.domain.review.repository.ReviewPhotoRepository;
+import org.sopt.snappinserver.domain.review.repository.ReviewRepository;
+import org.sopt.snappinserver.domain.review.service.dto.response.GetReviewDetailResult;
+import org.sopt.snappinserver.domain.review.service.usecase.GetReviewDetailUseCase;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+@Transactional(readOnly = true)
+public class GetReviewDetailService implements GetReviewDetailUseCase {
+
+    private final ReviewRepository reviewRepository;
+    private final ReviewPhotoRepository reviewPhotoRepository;
+    private static final ZoneId KOREA_ZONE = ZoneId.of("Asia/Seoul");
+
+    @Override
+    public GetReviewDetailResult getReviewDetail(Long userId, Long reviewId) {
+
+        Review review = reviewRepository.findById(reviewId)
+            .orElseThrow(() -> new ReviewException(ReviewErrorCode.REVIEW_NOT_FOUND));
+
+        List<String> images = reviewPhotoRepository
+            .findAllByReviewIds(List.of(reviewId))
+            .stream()
+            .map(rp -> rp.getPhoto().getImageUrl())
+            .toList();
+
+        return new GetReviewDetailResult(
+            review.getId(),
+            review.getReservation().getUser().getName(),
+            review.getRating(),
+            review.getCreatedAt().atZone(KOREA_ZONE).toLocalDate(),
+            images,
+            review.getContent()
+        );
+    }
+}
