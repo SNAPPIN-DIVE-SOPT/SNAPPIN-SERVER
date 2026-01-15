d3ad3369f82c7d80145d188cea006335c513df1c
earl9rey <earl9rey@naver.com>
2026-01-15 20:14:35 +0900

    style(#118/reservation): 불필요한 import문 제거

--- src/main/java/org/sopt/snappinserver/domain/reservation/service/dto/request/ExtraPriceCommand.java
+++ src/main/java/org/sopt/snappinserver/domain/reservation/service/dto/request/ExtraPriceCommand.java
@@ -1,15 +1,10 @@
 package org.sopt.snappinserver.domain.reservation.service.dto.request;
 
 import org.sopt.snappinserver.api.reservation.dto.request.ExtraPriceResult;
-import org.sopt.snappinserver.api.reservation.dto.request.RequestPaymentReservationRequest;
 
 public record ExtraPriceCommand(String name, int amount) {
 
-    public static ExtraPriceCommand from(ExtraPriceResult extraPrice)
-    {
-        return new ExtraPriceCommand(
-            extraPrice.name(),
-            extraPrice.amount()
-        );
+    public static ExtraPriceCommand from(ExtraPriceResult extraPrice) {
+        return new ExtraPriceCommand(extraPrice.name(), extraPrice.amount());
     }
 }
