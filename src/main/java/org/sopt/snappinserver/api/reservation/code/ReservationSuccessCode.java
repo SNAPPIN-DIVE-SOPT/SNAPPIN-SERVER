08dddb5020e97be4f50f8d514e9db49a37420a14
earl9rey <earl9rey@naver.com>
2026-01-15 18:03:15 +0900

    fix(#118/reservation): 예약 관련 성공 코드 넘버링 중복 제거

--- src/main/java/org/sopt/snappinserver/api/reservation/code/ReservationSuccessCode.java
+++ src/main/java/org/sopt/snappinserver/api/reservation/code/ReservationSuccessCode.java
@@ -36,17 +36,17 @@ public enum ReservationSuccessCode implements SuccessCode {
     ),
     PATCH_RESERVATION_CONFIRM_OK(
         200,
-        "RESERVATION_200_005",
+        "RESERVATION_200_006",
         "예약 확정 처리에 성공했습니다."
     ),
     PATCH_RESERVATION_REFUSE_OK(
         200,
-        "RESERVATION_200_006",
+        "RESERVATION_200_007",
         "예약 거절 처리에 성공했습니다."
     ),
     PATCH_RESERVATION_REQUEST_PAYMENT_OK(
         200,
-        "RESERVATION_200_006",
+        "RESERVATION_200_008",
         "결제 요청 처리에 성공했습니다."
     ),
 
