de95b1ce0b844831b3cd10329858d37baf32d31d
kaswhy <sxshx818@naver.com>
2026-01-16 04:23:03 +0900

    feat(#131/portfolio): 포트폴리오 목록 조회 컨트롤러 코드 작성

--- src/main/java/org/sopt/snappinserver/api/portfolio/controller/PortfolioController.java
+++ src/main/java/org/sopt/snappinserver/api/portfolio/controller/PortfolioController.java
@@ -4,19 +4,26 @@ import static org.sopt.snappinserver.api.portfolio.code.PortfolioSuccessCode.GET
 
 import lombok.RequiredArgsConstructor;
 import org.sopt.snappinserver.api.portfolio.code.PortfolioSuccessCode;
+import org.sopt.snappinserver.api.portfolio.dto.request.GetPortfolioListRequest;
 import org.sopt.snappinserver.api.portfolio.dto.response.GetCurationResponse;
 import org.sopt.snappinserver.api.portfolio.dto.response.GetPopularPortfolioListResponse;
 import org.sopt.snappinserver.api.portfolio.dto.response.GetPortfolioDetailResponse;
+import org.sopt.snappinserver.api.portfolio.dto.response.GetPortfolioListResponse;
+import org.sopt.snappinserver.api.portfolio.dto.response.GetPortfolioMetaResponse;
 import org.sopt.snappinserver.domain.auth.infra.jwt.CustomUserInfo;
+import org.sopt.snappinserver.domain.portfolio.service.dto.request.GetPortfolioListQuery;
 import org.sopt.snappinserver.domain.portfolio.service.dto.response.GetCuratedPortfolioResult;
 import org.sopt.snappinserver.domain.portfolio.service.dto.response.GetPopularPortfolioListResult;
 import org.sopt.snappinserver.domain.portfolio.service.dto.response.GetPortfolioDetailResult;
+import org.sopt.snappinserver.domain.portfolio.service.dto.response.GetPortfolioListResult;
 import org.sopt.snappinserver.domain.portfolio.service.usecase.GetCuratedPortfolioUseCase;
 import org.sopt.snappinserver.domain.portfolio.service.usecase.GetPopularPortfolioListUseCase;
 import org.sopt.snappinserver.domain.portfolio.service.usecase.GetPortfolioDetailUseCase;
+import org.sopt.snappinserver.domain.portfolio.service.usecase.GetPortfolioListUseCase;
 import org.sopt.snappinserver.global.response.dto.ApiResponseBody;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,6 +36,7 @@ public class PortfolioController implements PortfolioApi {
     private final GetPopularPortfolioListUseCase getPopularPortfolioListUseCase;
     private final GetPortfolioDetailUseCase getPortfolioDetailUseCase;
     private final GetCuratedPortfolioUseCase getCuratedPortfolioUseCase;
+    private final GetPortfolioListUseCase getPortfolioListUseCase;
 
     @Override
     @GetMapping("/popular")
@@ -67,5 +75,33 @@ public class PortfolioController implements PortfolioApi {
         GetCurationResponse response = GetCurationResponse.from(result);
 
         return ApiResponseBody.ok(PortfolioSuccessCode.GET_CURATED_PORTFOLIO_OK, response);
+    }
+
+    @Override
+    @GetMapping
+    public ApiResponseBody<GetPortfolioListResponse, GetPortfolioMetaResponse> getPortfolioList(
+        @ModelAttribute GetPortfolioListRequest request
+    ) {
+        GetPortfolioListQuery query = getQuery(request);
+        GetPortfolioListResult result = getPortfolioListUseCase.getPortfolioList(query);
+        GetPortfolioListResponse response = GetPortfolioListResponse.from(result);
+        GetPortfolioMetaResponse meta = GetPortfolioMetaResponse.from(
+            result.getPortfolioListMeta()
+        );
+
+        return ApiResponseBody.ok(PortfolioSuccessCode.GET_PORTFOLIO_LIST_OK, response, meta);
+    }
+
+    private GetPortfolioListQuery getQuery(GetPortfolioListRequest request) {
+        return new GetPortfolioListQuery(
+            request.moodIds(),
+            request.productId(),
+            request.photographerId(),
+            request.snapCategory(),
+            request.placeId(),
+            request.date(),
+            request.peopleCount(),
+            request.cursor()
+        );
     }
 }
---
+++ src/main/java/org/sopt/snappinserver/api/portfolio/dto/request/GetPortfolioListRequest.java
@@ -0,0 +1,42 @@
+package org.sopt.snappinserver.api.portfolio.dto.request;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Positive;
+import java.time.LocalDate;
+import java.util.List;
+import org.sopt.snappinserver.global.enums.SnapCategory;
+
+@Schema(description = "포폴 목록 조회 요청 DTO - 쿼리 파라미터용")
+@Valid
+public record GetPortfolioListRequest(
+
+    @Schema(description = "무드 아이디 목록(,로 구분, 개수 제한 없음)")
+    List<Long> moodIds,
+
+    @Schema(description = "상품 ID")
+    @Positive(message = "상품 ID는 양수값이어야 합니다.")
+    Long productId,
+
+    @Schema(description = "스냅 작가 ID")
+    @Positive(message = "스냅 작가 ID는 양수값이어야 합니다.")
+    Long photographerId,
+
+    @Schema(description = "촬영 상황 (스냅 유형)")
+    SnapCategory snapCategory,
+
+    @Schema(description = "장소 ID")
+    @Positive(message = "장소 ID는 양수값이어야 합니다.")
+    Long placeId,
+
+    @Schema(description = "촬영 일정 (YYYY-MM-DD)")
+    LocalDate date,
+
+    @Schema(description = "촬영 인원")
+    Integer peopleCount,
+
+    @Schema(description = "커서 값")
+    Long cursor
+) {
+
+}
