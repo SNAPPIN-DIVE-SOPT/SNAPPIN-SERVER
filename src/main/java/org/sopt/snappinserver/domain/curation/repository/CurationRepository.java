c5238af11efa65c6189f65070c4b302bdcf0d94a
kaswhy <sxshx818@naver.com>
2026-01-15 11:57:00 +0900

    feat(#121/curation): 최근 큐레이션 결과 3개 조회 리포지토리 코드 구현

--- src/main/java/org/sopt/snappinserver/domain/curation/repository/CurationRepository.java
+++ src/main/java/org/sopt/snappinserver/domain/curation/repository/CurationRepository.java
@@ -1,10 +1,13 @@
 package org.sopt.snappinserver.domain.curation.repository;
 
+import java.util.List;
 import org.sopt.snappinserver.domain.curation.domain.entity.Curation;
+import org.sopt.snappinserver.domain.user.domain.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CurationRepository extends JpaRepository<Curation, Long> {
 
+    List<Curation> findTop3ByUserOrderByRankAscCreatedAtDesc(User user);
 }
