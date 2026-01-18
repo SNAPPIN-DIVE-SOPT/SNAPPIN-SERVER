bc517a83007eb30ee35668d278b48c3693e2137e
kaswhy <sxshx818@naver.com>
2026-01-18 22:14:14 +0900

    feat(#142/auth): 기존 유저 여부 컬럼 응답에 추가

--- src/main/java/org/sopt/snappinserver/api/v1/auth/dto/response/CreateKakaoLoginResponse.java
+++ src/main/java/org/sopt/snappinserver/api/v1/auth/dto/response/CreateKakaoLoginResponse.java
@@ -1,12 +1,22 @@
 package org.sopt.snappinserver.api.v1.auth.dto.response;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import org.sopt.snappinserver.domain.auth.service.dto.response.LoginResult;
 
 @Schema(description = "카카오 로그인 응답 DTO")
 public record CreateKakaoLoginResponse(
+
+    @Schema(description = "신규 가입 여부")
+    boolean isNew,
 
     @Schema(description = "인증 시 필요한 accessToken입니다. refreshToken은 쿠키로 내려드립니다.")
     String accessToken
 ) {
 
+    public static CreateKakaoLoginResponse from(LoginResult loginResult){
+        return new CreateKakaoLoginResponse(
+            loginResult.isNew(),
+            loginResult.accessToken()
+        );
+    }
 }
