ea6e5f042f61840b794497a99b323fc08924a5b1
earl9rey <earl9rey@naver.com>
2026-01-18 23:06:23 +0900

    fix(#127/reservation): 예약 목록/상세 조회 시 평균 별점 NPE 발생 해결

--- src/main/java/org/sopt/snappinserver/domain/reservation/service/dto/response/GetReservationDetailProductResult.java
+++ src/main/java/org/sopt/snappinserver/domain/reservation/service/dto/response/GetReservationDetailProductResult.java
@@ -6,7 +6,7 @@ public record GetReservationDetailProductResult(
     Long id,
     String imageUrl,
     String title,
-    double rate,
+    Double rate,
     int reviewCount,
     String photographer,
     int price,
