48a4e5081a1069b621023ca52ff85932f2573d0b
earl9rey <earl9rey@naver.com>
2026-01-19 17:32:31 +0900

    refactor(#160/reservation): 결제 요청 Service 결과에 추가 비용 목록 포함

--- src/main/java/org/sopt/snappinserver/domain/reservation/service/dto/response/RequestPaymentReservationResult.java
+++ src/main/java/org/sopt/snappinserver/domain/reservation/service/dto/response/RequestPaymentReservationResult.java
@@ -2,13 +2,12 @@ package org.sopt.snappinserver.domain.reservation.service.dto.response;
 
 import java.util.List;
 import org.sopt.snappinserver.domain.reservation.domain.enums.ReservationStatus;
-import org.sopt.snappinserver.domain.reservation.service.dto.request.ExtraPriceCommand;
 
 public record RequestPaymentReservationResult(
     Long reservationId,
     ReservationStatus status,
     int basePrice,
-    List<ExtraPriceCommand> extraPrices,
+    List<ExtraPriceResult> extraPrices,
     int totalPrice
 ) {
 }
