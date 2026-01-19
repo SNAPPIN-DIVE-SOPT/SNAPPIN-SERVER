39f89eefd97be31975d099ef708b7697a72abdaa
earl9rey <earl9rey@naver.com>
2026-01-19 16:21:59 +0900

    style(#147/product): 불필요한 주석 제거

--- src/main/java/org/sopt/snappinserver/domain/product/service/dto/response/ProductReviewResult.java
+++ src/main/java/org/sopt/snappinserver/domain/product/service/dto/response/ProductReviewResult.java
@@ -2,10 +2,8 @@ package org.sopt.snappinserver.domain.product.service.dto.response;
 
 import java.time.LocalDate;
 import java.time.ZoneId;
-import java.util.Comparator;
 import java.util.List;
 import org.sopt.snappinserver.domain.review.domain.entity.Review;
-import org.sopt.snappinserver.domain.review.domain.entity.ReviewPhoto;
 
 public record ProductReviewResult(
     Long id,
@@ -42,10 +40,4 @@ public record ProductReviewResult(
             .toLocalDate();
     }
 
-//    private static List<String> extractImages(List<ReviewPhoto> reviewPhotos) {
-//        return reviewPhotos.stream()
-//            .sorted(Comparator.comparingInt(ReviewPhoto::getDisplayOrder))
-//            .map(reviewPhoto -> reviewPhoto.getPhoto().getImageUrl())
-//            .toList();
-//    }
 }
