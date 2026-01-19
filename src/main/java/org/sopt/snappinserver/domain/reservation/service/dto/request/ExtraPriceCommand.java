05263b1083431a05c660c899bdb001f2425b14d4
earl9rey <earl9rey@naver.com>
2026-01-19 17:32:56 +0900

    refactor(#160/reservation): 추가 비용 요청을 Command로 변환하는 팩토리 메서드 정리

--- src/main/java/org/sopt/snappinserver/domain/reservation/service/dto/request/ExtraPriceCommand.java
+++ src/main/java/org/sopt/snappinserver/domain/reservation/service/dto/request/ExtraPriceCommand.java
@@ -4,7 +4,10 @@ import org.sopt.snappinserver.api.v1.reservation.dto.request.ExtraPriceRequest;
 
 public record ExtraPriceCommand(String name, int amount) {
 
-    public static ExtraPriceCommand from(ExtraPriceRequest extraPrice) {
-        return new ExtraPriceCommand(extraPrice.name(), extraPrice.amount());
+    public static ExtraPriceCommand from(ExtraPriceRequest request) {
+        return new ExtraPriceCommand(
+            request.name(),
+            request.amount()
+        );
     }
 }
