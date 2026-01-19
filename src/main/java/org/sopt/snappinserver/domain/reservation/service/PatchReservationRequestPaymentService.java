eb54a54445e3fa0c779b172b887cd05f330ddf90
earl9rey <earl9rey@naver.com>
2026-01-19 14:49:08 +0900

    fix(#160/reservation): 결제 요청 응답애서 추가 비용을 목록 배열로 변경

--- src/main/java/org/sopt/snappinserver/domain/reservation/service/PatchReservationRequestPaymentService.java
+++ src/main/java/org/sopt/snappinserver/domain/reservation/service/PatchReservationRequestPaymentService.java
@@ -42,7 +42,7 @@ public class PatchReservationRequestPaymentService implements
             reservation.getId(),
             reservation.getReservationStatus(),
             command.basePrice(),
-            extraTotal,
+            command.extraPrices(),
             command.totalPrice()
         );
     }
--- src/main/java/org/sopt/snappinserver/domain/reservation/service/dto/response/RequestPaymentReservationResult.java
+++ src/main/java/org/sopt/snappinserver/domain/reservation/service/dto/response/RequestPaymentReservationResult.java
@@ -1,13 +1,15 @@
 package org.sopt.snappinserver.domain.reservation.service.dto.response;
 
+import java.util.List;
 import org.sopt.snappinserver.domain.reservation.domain.enums.ReservationStatus;
+import org.sopt.snappinserver.domain.reservation.service.dto.request.ExtraPriceCommand;
 
 public record RequestPaymentReservationResult(
     Long reservationId,
     ReservationStatus status,
     int basePrice,
-    int extraPrice,
+    List<ExtraPriceCommand> extraPrices,
     int totalPrice
 ) {
-
 }
+
