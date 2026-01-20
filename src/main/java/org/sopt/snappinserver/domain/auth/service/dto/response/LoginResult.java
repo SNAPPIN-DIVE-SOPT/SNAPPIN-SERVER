0489931dbd34dd8bb30702320343b27c0e16535d
kaswhy <sxshx818@naver.com>
2026-01-20 21:33:33 +0900

    fix(#180/auth): 로그인 시 현재 로그인한 유저 역할 반환

--- src/main/java/org/sopt/snappinserver/domain/auth/service/dto/response/LoginResult.java
+++ src/main/java/org/sopt/snappinserver/domain/auth/service/dto/response/LoginResult.java
@@ -1,10 +1,21 @@
 package org.sopt.snappinserver.domain.auth.service.dto.response;
 
 import org.sopt.snappinserver.domain.auth.domain.value.TokenPair;
+import org.sopt.snappinserver.domain.user.domain.enums.UserRole;
 
-public record LoginResult(boolean isNew, String accessToken, String refreshToken) {
+public record LoginResult(
+    boolean isNew,
+    String accessToken,
+    String refreshToken,
+    String role
+) {
 
-    public static LoginResult of(boolean isNew, TokenPair tokenPair) {
-        return new LoginResult(isNew, tokenPair.accessToken(), tokenPair.refreshToken());
+    public static LoginResult of(boolean isNew, TokenPair tokenPair, UserRole userRole) {
+        return new LoginResult(
+            isNew,
+            tokenPair.accessToken(),
+            tokenPair.refreshToken(),
+            userRole.name()
+        );
     }
 }
