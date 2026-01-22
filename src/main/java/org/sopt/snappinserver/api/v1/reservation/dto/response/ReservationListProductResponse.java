0e42ea2157745b956472699f8b0cf387a5b43b55
earl9rey <earl9rey@naver.com>
2026-01-22 23:53:08 +0900

    fix(#223/reservation): 예약 목록 조회 시 평균 별점 응답 포맷을 소수점 1자리로 고정

--- src/main/java/org/sopt/snappinserver/api/v1/reservation/dto/response/ReservationListProductResponse.java
+++ src/main/java/org/sopt/snappinserver/api/v1/reservation/dto/response/ReservationListProductResponse.java
@@ -1,6 +1,8 @@
 package org.sopt.snappinserver.api.v1.reservation.dto.response;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.List;
 import org.sopt.snappinserver.domain.reservation.service.dto.response.GetReservationListProductResult;
 
@@ -17,7 +19,7 @@ public record ReservationListProductResponse(
     String title,
 
     @Schema(description = "평균 별점", example = "4.7")
-    Double rate,
+    BigDecimal rate,
 
     @Schema(description = "리뷰 개수", example = "20")
     int reviewCount,
@@ -42,7 +44,8 @@ public record ReservationListProductResponse(
             result.id(),
             result.imageUrl(),
             result.title(),
-            result.rate(),
+            BigDecimal.valueOf(result.rate())
+                .setScale(1, RoundingMode.HALF_UP),
             result.reviewCount(),
             result.photographer(),
             result.price(),
