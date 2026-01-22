9dea33c80c4f759971803c780fefed73b8055418
earl9rey <earl9rey@naver.com>
2026-01-22 23:56:09 +0900

    fix(#223/product): 상품 목록 조회 시 평균 별점 응답 포맷을 소수점 1자리로 고정

--- src/main/java/org/sopt/snappinserver/api/v1/product/dto/response/GetProductCardResponse.java
+++ src/main/java/org/sopt/snappinserver/api/v1/product/dto/response/GetProductCardResponse.java
@@ -1,6 +1,8 @@
 package org.sopt.snappinserver.api.v1.product.dto.response;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.List;
 import org.sopt.snappinserver.domain.product.service.dto.response.GetProductCardResult;
 
@@ -17,7 +19,7 @@ public record GetProductCardResponse(
     String title,
 
     @Schema(description = "평균 별점")
-    Double rate,
+    BigDecimal rate,
 
     @Schema(description = "리뷰 개수")
     long reviewCount,
@@ -37,7 +39,8 @@ public record GetProductCardResponse(
             result.id(),
             result.imageUrl(),
             result.title(),
-            result.rate(),
+            BigDecimal.valueOf(result.rate())
+                .setScale(1, RoundingMode.HALF_UP),
             result.reviewCount(),
             result.photographer(),
             result.price(),
