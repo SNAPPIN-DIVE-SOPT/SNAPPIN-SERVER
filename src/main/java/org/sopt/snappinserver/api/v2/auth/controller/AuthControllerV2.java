531ca4064a698cc8366349bb3f4e0f75c62971cd
kaswhy <sxshx818@naver.com>
2026-02-19 21:54:32 +0900

    refactor(#243/auth): 카카오 로그인 v2 로직 추가

---
+++ src/main/java/org/sopt/snappinserver/api/v2/auth/controller/AuthControllerV2.java
@@ -0,0 +1,72 @@
+package org.sopt.snappinserver.api.v2.auth.controller;
+
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.sopt.snappinserver.api.v2.auth.dto.request.CreateKakaoLoginRequest;
+import org.sopt.snappinserver.api.v2.auth.dto.response.CreateKakaoLoginResponse;
+import org.sopt.snappinserver.domain.auth.facade.AuthFacade;
+import org.sopt.snappinserver.domain.auth.service.dto.response.LoginWithPhotographerProfileResult;
+import org.sopt.snappinserver.global.response.code.auth.AuthSuccessCode;
+import org.sopt.snappinserver.global.response.dto.ApiResponseBody;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/api/v2/auth")
+@RequiredArgsConstructor
+@RestController
+@Slf4j
+public class AuthControllerV2 implements AuthApi {
+
+    private final AuthFacade authFacade;
+
+    @Value("${auth.cookie.secure}")
+    private boolean isSecure;
+
+    @Value("${jwt.refresh-token-ttl-seconds}")
+    private long refreshTokenSeconds;
+
+    @Override
+    @PostMapping("/login/kakao")
+    public ApiResponseBody<CreateKakaoLoginResponse, Void> createKakaoLogin(
+        @RequestParam(name = "redirect_uri", required = false) String clientRedirectUri,
+        @Valid @RequestBody CreateKakaoLoginRequest createKakaoLoginRequest,
+        @RequestHeader(value = "User-Agent", required = false) String userAgent,
+        HttpServletResponse httpServletResponse
+    ) {
+        String redirectUri =
+            (clientRedirectUri == null)
+                ? ("http://localhost:8080/api/v1/auth/login/kakao")
+                : clientRedirectUri;
+        LoginWithPhotographerProfileResult loginResult = authFacade.loginWithPhotographerProfile(
+            redirectUri,
+            createKakaoLoginRequest.code(),
+            userAgent
+        );
+        ResponseCookie refreshCookie = getResponseCookie(loginResult.refreshToken());
+        httpServletResponse.addHeader(HttpHeaders.SET_COOKIE, refreshCookie.toString());
+
+        return ApiResponseBody.ok(
+            AuthSuccessCode.LOGIN_SUCCESS,
+            CreateKakaoLoginResponse.from(loginResult)
+        );
+    }
+
+    private ResponseCookie getResponseCookie(String refreshTokenValue) {
+        return ResponseCookie.from("refreshToken", refreshTokenValue)
+            .httpOnly(true)
+            .secure(isSecure)
+            .sameSite(isSecure ? "None" : "Lax")
+            .path("/")
+            .maxAge(refreshTokenSeconds)
+            .build();
+    }
+}
