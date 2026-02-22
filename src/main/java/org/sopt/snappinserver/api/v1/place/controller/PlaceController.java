ffcde2aa6d6e494bf415485bf2618211cf894907
kaswhy <sxshx818@naver.com>
2026-02-22 22:31:02 +0900

    refactor(#249/place): place 도메인 v1 api annotation 위치 재정비

--- src/main/java/org/sopt/snappinserver/api/v1/place/controller/PlaceController.java
+++ src/main/java/org/sopt/snappinserver/api/v1/place/controller/PlaceController.java
@@ -2,12 +2,13 @@ package org.sopt.snappinserver.api.v1.place.controller;
 
 import static org.sopt.snappinserver.global.response.code.place.PlaceSuccessCode.GET_PLACE_LIST_OK;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.validator.constraints.Length;
 import org.sopt.snappinserver.api.v1.place.dto.response.GetPlaceListResponse;
 import org.sopt.snappinserver.domain.place.service.dto.response.GetPlaceListResult;
 import org.sopt.snappinserver.domain.place.service.usecase.GetPlaceListUseCase;
 import org.sopt.snappinserver.global.response.dto.ApiResponseBody;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,8 +20,9 @@ public class PlaceController implements PlaceApi {
     private final GetPlaceListUseCase getPlaceListUseCase;
 
     @Override
-    @GetMapping()
     public ApiResponseBody<GetPlaceListResponse, Void> getPlaces(
+        @NotBlank(message = "검색 키워드는 필수입니다.")
+        @Length(max = 32, message = "검색 키워드는 최대 32자까지 입력할 수 있습니다.")
         String keyword
     ) {
         GetPlaceListResult result = getPlaceListUseCase.getPlaceList(keyword);
