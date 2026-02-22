c9c82d227b3b6afc430c13145ea96167f50d6bb0
kaswhy <sxshx818@naver.com>
2026-02-22 22:25:51 +0900

    refactor(#249/photo): photo 도메인 v1 api annotation 위치 재정비

--- src/main/java/org/sopt/snappinserver/api/v1/photo/controller/PhotoController.java
+++ src/main/java/org/sopt/snappinserver/api/v1/photo/controller/PhotoController.java
@@ -1,14 +1,11 @@
 package org.sopt.snappinserver.api.v1.photo.controller;
 
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.sopt.snappinserver.global.response.code.photo.PhotoSuccessCode;
 import org.sopt.snappinserver.api.v1.photo.dto.request.CreatePhotoMoodRequest;
 import org.sopt.snappinserver.domain.photo.service.dto.request.PhotoProcessCommand;
 import org.sopt.snappinserver.domain.photo.service.usecase.ProcessPhotoUseCase;
+import org.sopt.snappinserver.global.response.code.photo.PhotoSuccessCode;
 import org.sopt.snappinserver.global.response.dto.ApiResponseBody;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,9 +17,8 @@ public class PhotoController implements PhotoApi {
     private final ProcessPhotoUseCase processPhotoUseCase;
 
     @Override
-    @PostMapping("/process")
     public ApiResponseBody<Void, Void> createPhotoMoodConnection(
-        @Valid @RequestBody CreatePhotoMoodRequest createPhotoMoodRequest
+        CreatePhotoMoodRequest createPhotoMoodRequest
     ) {
         PhotoProcessCommand photoProcessCommand = PhotoProcessCommand.from(createPhotoMoodRequest);
         processPhotoUseCase.linkPhotoWithMoodTags(photoProcessCommand);
