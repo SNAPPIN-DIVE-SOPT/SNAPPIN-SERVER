2d0b2757802b0f7398d371f5fadfa29bc416e00d
kaswhy <sxshx818@naver.com>
2026-04-01 06:45:46 +0900

    feat(#263/user): 온보딩 완료 여부 조회 서비스 코드 추가

---
+++ src/main/java/org/sopt/snappinserver/domain/user/service/GetOnboardingExistsService.java
@@ -0,0 +1,28 @@
+package org.sopt.snappinserver.domain.user.service;
+
+import lombok.RequiredArgsConstructor;
+import org.sopt.snappinserver.domain.user.domain.entity.User;
+import org.sopt.snappinserver.domain.user.domain.exception.UserErrorCode;
+import org.sopt.snappinserver.domain.user.domain.exception.UserException;
+import org.sopt.snappinserver.domain.user.repository.OnboardingRepository;
+import org.sopt.snappinserver.domain.user.repository.UserRepository;
+import org.sopt.snappinserver.domain.user.service.dto.response.GetOnboardingExistsResult;
+import org.sopt.snappinserver.domain.user.service.usecase.GetOnboardingExistsUseCase;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+@Service
+public class GetOnboardingExistsService implements GetOnboardingExistsUseCase {
+
+    private final UserRepository userRepository;
+    private final OnboardingRepository onboardingRepository;
+
+    @Override
+    public GetOnboardingExistsResult getOnboardingExists(Long userId) {
+        User user = userRepository.findById(userId)
+            .orElseThrow(() -> new UserException(UserErrorCode.USER_NOT_FOUND));
+        return new GetOnboardingExistsResult(onboardingRepository.existsByUser(user));
+    }
+}
---
+++ src/main/java/org/sopt/snappinserver/domain/user/service/dto/response/GetOnboardingExistsResult.java
@@ -0,0 +1,7 @@
+package org.sopt.snappinserver.domain.user.service.dto.response;
+
+public record GetOnboardingExistsResult(
+    boolean isOnboardingCompleted
+) {
+
+}
