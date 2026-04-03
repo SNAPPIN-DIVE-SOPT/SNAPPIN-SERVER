9f6aba2cd806ec00d6377e0a6f83eedc65e06263
earl9rey <earl9rey@naver.com>
2026-04-03 21:28:51 +0900

    feat(#264/wish): 위시 상품 목록 조회 v2 API 인터페이스 구현

---
+++ src/main/java/org/sopt/snappinserver/api/v2/wish/controller/WishApi.java
@@ -0,0 +1,28 @@
+package org.sopt.snappinserver.api.v2.wish.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.sopt.snappinserver.api.v2.wish.dto.response.WishedProductsMetaResponse;
+import org.sopt.snappinserver.api.v2.wish.dto.response.WishedProductsResponse;
+import org.sopt.snappinserver.domain.auth.infra.jwt.CustomUserInfo;
+import org.sopt.snappinserver.global.response.dto.ApiResponseBody;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Tag(name = "012 - Wish (v2)", description = "좋아요 관련 API (v2)")
+public interface WishApi {
+
+    @Operation(
+        summary = "위시 상품 목록 조회",
+        description = "커서 기반 페이지네이션으로 사용자가 좋아요한 상품 목록을 조회합니다. 페이지 크기는 10입니다."
+    )
+    @GetMapping("/products")
+    ApiResponseBody<WishedProductsResponse, WishedProductsMetaResponse> getWishedProducts(
+
+        @Parameter(hidden = true)
+        CustomUserInfo userInfo,
+
+        Long cursor
+    );
+
+}
