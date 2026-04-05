2f3cd6d8e1be2520de83d6df9e42d2c0c70f914a
earl9rey <earl9rey@naver.com>
2026-04-06 00:09:21 +0900

    refactor(#279/review): 상품 기반 리뷰 등록 성공 코드 추가

--- src/main/java/org/sopt/snappinserver/global/response/code/product/ProductSuccessCode.java
+++ src/main/java/org/sopt/snappinserver/global/response/code/product/ProductSuccessCode.java
@@ -20,7 +20,8 @@ public enum ProductSuccessCode implements SuccessCode {
     GET_POPULAR_MOOD_PRODUCTS_OK(200, "PRODUCT_200_009", "인기 무드 상품 목록 조회에 성공했습니다."),
 
     // 201 CREATED
-    POST_PRODUCT_RESERVATION_OK(201, "PRODUCT_201_001", "상품 예약 생성에 성공했습니다.");
+    POST_PRODUCT_RESERVATION_OK(201, "PRODUCT_201_001", "상품 예약 생성에 성공했습니다."),
+    POST_PRODUCT_REVIEW_CREATED(201, "PRODUCT_201_002", "상품 리뷰 등록에 성공했습니다.");
 
     private final int status;
     private final String code;
